cbba4aa0f4d2b628497c160e05711df0b714c124
Lee Dong Wook <sh95119@gmail.com>
2021-01-26 00:02:52 +0900

    FEATURE: add TaskController, Task class implement get tasks, post task method

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,48 @@
+// TODO
+// 1. READ Collection - GET /tasks
+// 2. READ Item - Get / tasks/{id}
+// 3. Create - POST /tasks
+// 4. Update - PUT/PATCH /tasks/{id}
+// 5. DELETE /tasks/{id}
+
+package com.codesoom.assignment.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesoom.assignment.model.Task;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
+    @GetMapping
+    public List<Task> list() {
+        return tasks;
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        if (task.getTitle().isBlank()) {
+            // TODO: validation error....
+        }
+        task.setId(generateId());
+        tasks.add(task);
+
+        return task;
+    }
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
+    }
+}
---
+++ app/src/main/java/com/codesoom/assignment/model/Task.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.model;
+
+public class Task {
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: {} title: {}", id, title);
+    }
+}
