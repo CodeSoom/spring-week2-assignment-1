770a2bd98c0f215138039a36d65043dccf069527
jennifer <jennifer@novonetworks.com>
2021-01-25 20:20:14 +0900

    Implement get

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,10 +1,13 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.NotFoundException;
 import com.codesoom.assignment.models.Task;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,6 +18,12 @@ public class TaskController {
     @GetMapping
     public List<Task> list() {
         return tasks;
+    }
+
+    @GetMapping("/{id}")
+    public Task get(@PathVariable Long id) throws IOException {
+        Task task = findById(id);
+        return task;
     }
 
     @PostMapping
@@ -28,5 +37,12 @@ public class TaskController {
     private Long generateId() {
         newId += 1;
         return newId;
+    }
+
+    private Task findById(Long id) throws IOException {
+        Optional<Task> task = tasks.stream().filter(i -> i.getId().equals(id)).findFirst();
+
+        if (task.isEmpty()) throw new NotFoundException();
+        return task.get();
     }
 }
