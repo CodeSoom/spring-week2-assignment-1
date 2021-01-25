5f81693fc0557bcbb97d7e5d484b757c8efdc6c9
wenodev <logicalstark1105@gmail.com>
2021-01-26 01:08:42 +0900

    devtools 추가

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String sayHello() {
-        return "Hello, Codesoom!!";
+        return "Hello, Codesoom!";
     }
 }
