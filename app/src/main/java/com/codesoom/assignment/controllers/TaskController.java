c24ce52b20782b2cc7c76df3702119482fd1b0df
etff <wish2rich@gmail.com>
2021-01-25 21:54:58 +0900

    feature: add get tasks

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,4 +1,21 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.domain.Task;
+import com.codesoom.assignment.domain.Tasks;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    private Tasks tasks = new Tasks();
+    private Long id = 0L;
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return tasks.getTasks();
+    }
 }
