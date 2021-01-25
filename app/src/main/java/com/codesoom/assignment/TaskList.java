9af258e260c204745ee63a678f01e123f93ce407
newoo <newoo4297@naver.com>
2021-01-25 23:08:12 +0900

    Create TaskList to handle CRUD of task collection

---
+++ app/src/main/java/com/codesoom/assignment/TaskList.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class TaskList {
+    private List<Task> taskList = new ArrayList<Task>();
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public Task getTask(Long id) {
+        return taskList.stream().filter(task -> Objects.equals(id, task.getId())).findFirst().orElse(new Task());
+    }
+
+    public Task add(String title) {
+        Long newId = Long.valueOf(taskList.size());
+        Task task = new Task(newId, title);
+        return taskList.add(task) ? task : null;
+    }
+
+    public Task modify(Long id, String title) {
+        Task task = getTask(id);
+        int index = taskList.indexOf(task);
+
+        if (index < 0) {
+            return null;
+        }
+
+        task.setTitle(title);
+        return taskList.set(index, task);
+    }
+
+    public boolean remove(Long id) {
+        return taskList.removeIf(task -> Objects.equals(id, task.getId()));
+    }
+}
