465fef9427f511320c589ccc61fbec48fd4a996e
Las <haho013039@gmail.com>
2021-01-26 21:23:12 +0900

    Implement update task title using PUT

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -35,5 +37,21 @@ public class TaskController {
     public String createTask(@RequestBody JsonTask jsonTask) {
         Long createdTaskId = taskApplicationService.createTask(jsonTask.title);
         return taskApplicationService.findTask(createdTaskId).flatMap(it -> transfer.taskToJson(it)).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTitle(@PathVariable Long id, @RequestBody JsonTask jsonTask) {
+        Optional<Object> result = taskApplicationService.updateTaskTitle(id, jsonTask.title);
+        if (result.isEmpty()) {
+            new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return taskApplicationService.findTask(id)
+            .flatMap(
+                it -> transfer.taskToJson(it)
+            ).map(
+                it -> new ResponseEntity<>(it, HttpStatus.OK)
+            ).orElse(
+                new ResponseEntity<>(null, HttpStatus.NOT_FOUND)
+            );
     }
 }
