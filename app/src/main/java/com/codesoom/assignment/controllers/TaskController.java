ca8aac6a392451b76963bf5dec172cacffb49f19
developerOlive <chloehiho@gmail.com>
2021-01-27 04:41:04 +0900

    Refactor : 할 일을 담는 데이터가 다량일 경우를 대비하여 구조를 List에서 Map형식으로 변경, npm 테스트 통과 체킹

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,26 +5,25 @@ import com.codesoom.assignment.models.Task;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
-    private List<Task> tasks = new ArrayList<>();
+    private static Map<Long, Task> taskStore = new HashMap<>();
     private Long newId = 0L;
     NotFoundException notFoundException = new NotFoundException();
 
     @GetMapping
     public List<Task> getTaskList() {
-        return tasks;
+        return new ArrayList<>(taskStore.values());
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Task getTaskById(@PathVariable Long id) {
-        if (findTask(id) == null) {
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        if (findTask(id).isEmpty()) {
             throw notFoundException;
         }
         return findTask(id);
@@ -34,28 +33,28 @@ public class TaskController {
     @ResponseStatus(HttpStatus.CREATED)
     public Task handleCreate(@RequestBody Task task) {
         task.setId(generateId());
-        tasks.add(task);
+        taskStore.put(task.getId(), task);
         return task;
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Task handleUpdate(@PathVariable Long id, @RequestBody Task task) {
-        if (findTask(id) == null) {
+        if (findTask(id).isEmpty()) {
             throw notFoundException;
         }
-        findTask(id).setTitle(task.getTitle());
+        taskStore.replace(task.getId(), task);
         return task;
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void handleDelete(@PathVariable Long id) {
-        if (findTask(id) == null) {
+        if (findTask(id).isEmpty()) {
             throw notFoundException;
         }
-        Task task = findTask(id);
-        tasks.remove(task);
+        Optional<Task> task = findTask(id);
+        taskStore.remove(id);
     }
 
     private Long generateId() {
@@ -63,11 +62,8 @@ public class TaskController {
         return newId;
     }
 
-    public Task findTask(Long id) {
-        return tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    public Optional<Task> findTask(Long id) {
+        return Optional.ofNullable(taskStore.get(id));
     }
 
 }
