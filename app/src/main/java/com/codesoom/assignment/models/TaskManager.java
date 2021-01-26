7e846bda5fabbe9d1152673996f6b25e16ed36fb
juunini <juuni.ni.i@gmail.com>
2021-01-26 18:46:51 +0900

    Set task manager

--- app/src/main/java/com/codesoom/assignment/models/TaskManager.java
+++ app/src/main/java/com/codesoom/assignment/models/TaskManager.java
@@ -1,4 +1,73 @@
 package com.codesoom.assignment.models;
 
+import com.codesoom.assignment.errors.NotFoundTaskIDException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TaskManager {
+    private static final TaskManager manager = new TaskManager();
+    private static final HashMap<Long, Task> tasks = new HashMap<>();
+    private static long newID = 0;
+
+    public static TaskManager getInstance() {
+        return manager;
+    }
+
+    private void increaseNewID() {
+        newID += 1;
+    }
+
+    private void initializationNewID() {
+        newID = 0;
+    }
+
+    private boolean isExistID(long id) {
+        Task task = tasks.remove(id);
+        return task != null;
+    }
+
+    public void clear() {
+        tasks.clear();
+        initializationNewID();
+    }
+
+    public List<Task> findAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    public Task findOne(long id) throws NotFoundTaskIDException {
+        Task task = tasks.get(id);
+        if (task == null) {
+            throw new NotFoundTaskIDException(id);
+        }
+        return task;
+    }
+
+    public Task insertOne(String title) {
+        Task task = new Task(newID, title);
+        increaseNewID();
+
+        tasks.put(task.id(), task);
+        return task;
+    }
+
+    public Task modifyOne(long id, String title) throws NotFoundTaskIDException {
+        if (!isExistID(id)) {
+            throw new NotFoundTaskIDException(id);
+        }
+
+        Task task = new Task(id, title);
+        tasks.replace(id, task);
+        return task;
+    }
+
+    public Task deleteOne(long id) throws NotFoundTaskIDException {
+        Task removedTask = tasks.remove(id);
+        if (removedTask == null) {
+            throw new NotFoundTaskIDException(id);
+        }
+        return removedTask;
+    }
 }
--- app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
@@ -1,21 +1,33 @@
 package com.codesoom.assignment.models;
 
+import com.codesoom.assignment.errors.NotFoundTaskIDException;
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @DisplayName("TaskManager 클래스")
 public class TaskManagerTest {
+    TaskManager taskManager = TaskManager.getInstance();
+
     @BeforeEach
     void BeforeEach() {
-        // clear 메소드를 실행하도록 하기
+        taskManager.clear();
     }
 
     @Nested
     @DisplayName("clear 메소드는")
     class Describe_clear {
+        List<Task> expectTasks = new ArrayList<>();
+
         @Test
         @DisplayName("아무것도 리턴하지 않는다. clear 후에 findAll 을 하면 빈 Task 리스트를 리턴한다.")
         void It_returns_nothing_and_find_all_returns_empty_task_list() {
+            taskManager.clear();
 
+            List<Task> tasks = taskManager.findAll();
+            Assertions.assertArrayEquals(expectTasks.toArray(), tasks.toArray());
         }
     }
 
@@ -26,22 +38,34 @@ public class TaskManagerTest {
         @Nested
         @DisplayName("Task 가 하나도 없을 때")
         class Context_when_task_is_empty {
+            List<Task> expectTasks = new ArrayList<>();
 
             @Test
             @DisplayName("빈 Task 리스트를 리턴한다.")
             void It_returns_empty_task_list() {
+                List<Task> tasks = taskManager.findAll();
 
+                Assertions.assertArrayEquals(expectTasks.toArray(), tasks.toArray());
             }
         }
 
         @Nested
         @DisplayName("Task 가 있을 때")
         class Context_when_task_is_exists {
+            long givenID = 0L;
+            String givenTitle = "sample";
+            Task givenTask = new Task(givenID, givenTitle);
+            List<Task> expectTasks = new ArrayList<>(Collections.singletonList(givenTask));
 
             @Test
             @DisplayName("Task 리스트를 리턴한다.")
             void It_returns_task_list() {
+                taskManager.insertOne(givenTitle);
+                List<Task> tasks = taskManager.findAll();
 
+                Assertions.assertEquals(expectTasks.toArray().length, tasks.toArray().length);
+                Assertions.assertEquals(expectTasks.get(0).id(), tasks.get(0).id());
+                Assertions.assertEquals(expectTasks.get(0).title(), tasks.get(0).title());
             }
         }
     }
@@ -52,22 +76,29 @@ public class TaskManagerTest {
         @Nested
         @DisplayName("찾으려는 id가 없을 때")
         class Context_when_find_id_is_not_exists {
+            long givenID = 0;
 
             @Test
             @DisplayName("NotFoundTaskIDException 을 던진다.")
             void It_throws_not_found_task_id_exception() {
-
+                Assertions.assertThrows(NotFoundTaskIDException.class, () -> taskManager.findOne(givenID));
             }
         }
 
         @Nested
         @DisplayName("찾으려는 id가 있을 때")
         class Context_when_find_id_is_exists {
+            long givenID = 0;
+            String givenTitle = "sample";
 
             @Test
             @DisplayName("Task 를 리턴한다.")
             void It_returns_task() {
+                taskManager.insertOne(givenTitle);
 
+                Task task = taskManager.findOne(givenID);
+
+                Assertions.assertEquals(givenTitle, task.title());
             }
         }
     }
@@ -75,17 +106,26 @@ public class TaskManagerTest {
     @Nested
     @DisplayName("insertOne 메소드는")
     class Describe_insertOne {
+        long givenID = 0;
+        String givenTitle = "sample";
 
         @Test
         @DisplayName("생성된 Task 를 리턴한다.")
         void It_returns_inserted_task() {
+            Task task = taskManager.insertOne(givenTitle);
 
+            Assertions.assertEquals(givenID, task.id());
+            Assertions.assertEquals(givenTitle, task.title());
         }
     }
 
     @Nested
     @DisplayName("modifyOne 메소드는")
     class Describe_modifyOne {
+        long givenID = 0;
+        String givenTitle = "sample";
+        String givenModifyTitle = "modified sample";
+
         @Nested
         @DisplayName("변경하려는 id가 없을 때")
         class Context_when_modify_id_is_not_exists {
@@ -93,7 +133,7 @@ public class TaskManagerTest {
             @Test
             @DisplayName("NotFoundTaskIDException 을 던진다.")
             void It_throws_not_found_task_id_exception() {
-
+                Assertions.assertThrows(NotFoundTaskIDException.class, () -> taskManager.modifyOne(givenID, givenModifyTitle));
             }
         }
 
@@ -104,7 +144,11 @@ public class TaskManagerTest {
             @Test
             @DisplayName("수정된 Task 를 리턴한다.")
             void It_returns_modified_task() {
+                taskManager.insertOne(givenTitle);
+                Task task = taskManager.modifyOne(givenID, givenModifyTitle);
 
+                Assertions.assertEquals(givenID, task.id());
+                Assertions.assertEquals(givenModifyTitle, task.title());
             }
         }
     }
@@ -112,6 +156,9 @@ public class TaskManagerTest {
     @Nested
     @DisplayName("deleteOne 메소드는")
     class Describe_deleteOne {
+        long givenID = 0;
+        String givenTitle = "sample";
+
         @Nested
         @DisplayName("삭제하려는 id가 없을 때")
         class Context_when_delete_id_is_not_exists {
@@ -119,7 +166,7 @@ public class TaskManagerTest {
             @Test
             @DisplayName("NotFoundTaskIDException 을 던진다.")
             void It_throws_not_found_task_id_exception() {
-
+                Assertions.assertThrows(NotFoundTaskIDException.class, () -> taskManager.deleteOne(givenID));
             }
         }
 
@@ -128,9 +175,14 @@ public class TaskManagerTest {
         class Context_when_delete_id_is_exists {
 
             @Test
-            @DisplayName("아무것도 리턴하지 않는다. 식제한 Task 를 찾으려 하면 NotFoundTaskIDException 을 던진다.")
-            void It_returns_nothing_and_find_id_throws_not_found_task_id_exception() {
+            @DisplayName("삭제된 Task 를 리턴한다. 식제한 Task 를 찾으려 하면 NotFoundTaskIDException 을 던진다.")
+            void It_returns_deleted_task_and_find_id_throws_not_found_task_id_exception() {
+                taskManager.insertOne(givenTitle);
+                Task task = taskManager.deleteOne(givenID);
 
+                Assertions.assertEquals(givenID, task.id());
+                Assertions.assertEquals(givenTitle, task.title());
+                Assertions.assertThrows(NotFoundTaskIDException.class, () -> taskManager.findOne(givenID));
             }
         }
     }
