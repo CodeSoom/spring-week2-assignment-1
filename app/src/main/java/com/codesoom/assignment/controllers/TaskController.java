6dfafa5e4a85ff88784e13c86a01e598f0fbc4a8
Lee Dong Wook <sh95119@gmail.com>
2021-01-27 01:23:00 +0900

    FEATURE: add web front with marko

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -10,6 +10,7 @@ package com.codesoom.assignment.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.codesoom.assignment.model.Task;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
 
     private List<Task> tasks = new ArrayList<>();
