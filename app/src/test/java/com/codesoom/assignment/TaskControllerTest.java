b2cc28793c40bf17e107c5d74b6a50987696bde7
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-26 15:05:50 +0900

    test: Apply BDD pattern to get task test code

--- app/src/test/java/com/codesoom/assignment/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/TaskControllerTest.java
@@ -1,100 +1,52 @@
 package com.codesoom.assignment;
 
-import com.codesoom.assignment.controllers.TaskController;
-import com.codesoom.assignment.domain.Task;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.checkerframework.dataflow.qual.TerminatesExecution;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.nio.charset.Charset;
-
-import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
 @RunWith(SpringRunner.class)
-@WebMvcTest(TaskController.class)
-public class TaskControllerTest {
-    public static final MediaType APPLICATION_JSON_UTF8 =
-            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@DisplayName("TaskController 클래스")
+class TaskControllerTest {
     @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+    MockMvc mockMvc;
 
-    @Test
-    public void testGetEmptyTaskList() throws Exception {
-        mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("[]"));
-    }
+    @Nested
+    @DisplayName("GET 메소드는")
+    class Describe_GET {
+        @Nested
+        @DisplayName("만약 tasks가 비어있다면")
+        class Context_empty_task {
+            @Test
+            @DisplayName("빈 배열을 리턴한다")
+            void it_returns_empty_array() throws Exception {
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("[]"));
+            }
+        }
 
-    @Test
-    public void testCreateTask() throws Exception {
-        Task expectedTask = new Task(0, "Get Sleep");
-        Task createdTask = createTask(expectedTask.getTitle());
-        assertEquals("id", expectedTask.getId(), createdTask.getId());
-        assertEquals("title", expectedTask.getTitle(), createdTask.getTitle());
-    }
-
-    @Test
-    public void testGetCreatedTask() throws Exception {
-        Task expectedTask = new Task(0, "Get Sleep");
-        Task createdTask = createTask(expectedTask.getTitle());
-
-        assertEquals("id", expectedTask.getId(), createdTask.getId());
-        assertEquals("title", expectedTask.getTitle(), createdTask.getTitle());
-
-        mockMvc.perform(get("/tasks/" + createdTask.getId()))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(createdTask)));
-    }
-
-    @Test
-    public void testUpdateTask() throws Exception {
-        Task expectedTask = new Task(0, "Get Sleep");
-        Task createdTask = createTask(expectedTask.getTitle());
-
-        assertEquals("id", expectedTask.getId(), createdTask.getId());
-        assertEquals("title", expectedTask.getTitle(), createdTask.getTitle());
-
-        MvcResult mvcResult = mockMvc.perform(put("/tasks/" + createdTask.getId()).contentType(APPLICATION_JSON_UTF8).content("{\"title\":\"Do Work\"}"))
-                .andExpect(status().isOk())
-                .andReturn();
-        Task updatedTask = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Task.class);
-        assertEquals("id", createdTask.getId(), updatedTask.getId());
-        assertEquals("title", "Do Work", updatedTask.getTitle());
-    }
-
-    @Test
-    public void testDeleteTask() throws Exception {
-        Task expectedTask = new Task(0, "Get Sleep");
-        Task createdTask = createTask(expectedTask.getTitle());
-
-        assertEquals("id", expectedTask.getId(), createdTask.getId());
-        assertEquals("title", expectedTask.getTitle(), createdTask.getTitle());
-
-        mockMvc.perform(delete("/tasks/" + createdTask.getId()))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/tasks/" + createdTask.getId()))
-                .andExpect(status().isNotFound());
-    }
-
-    private Task createTask(String title) throws Exception {
-        MvcResult mvcResult = mockMvc.perform(post("/tasks").contentType(APPLICATION_JSON_UTF8).content("{\"title\":\"" + title + "\"}"))
-                .andExpect(status().isOk())
-                .andReturn();
-        return objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Task.class);
+        @Nested
+        @DisplayName("만약 없는 task id를 요청한다면")
+        class Context_invalid_task_id {
+            @Test
+            @DisplayName("404 에러를 리턴한다")
+            void it_returns_404_error() throws Exception {
+                mockMvc.perform(get("/tasks/0"))
+                        .andExpect(status().isNotFound());
+            }
+        }
     }
 }
