d4b544da6dd68dc59bae6803fc444698066b3734
newoo <newoo4297@naver.com>
2021-01-26 17:11:28 +0900

    Create JsonParser to convert object to json

---
+++ app/src/main/java/com/codesoom/assignment/controllers/JsonParser.java
@@ -0,0 +1,17 @@
+package com.codesoom.assignment.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class JsonParser<T> {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public String toJson(T object) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, object);
+        return outputStream.toString();
+    }
+}
