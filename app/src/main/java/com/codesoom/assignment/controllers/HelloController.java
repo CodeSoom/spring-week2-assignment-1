6ee312e5e327d906106953cadae6a02d814b4b22
juunini <juuni.ni.i@gmail.com>
2021-01-26 12:38:32 +0900

    Add cors

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package com.codesoom.assignment.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloController {
     @RequestMapping("/")
     public String sayHello() {
