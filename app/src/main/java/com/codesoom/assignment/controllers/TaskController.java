7f5aa62853252669cd9d2e108ae2b2651ab4bb95
Gyu <melon9751@naver.com>
2021-01-26 19:28:08 +0900

    Make business logic in Controller
    Don't need ObjectMapper, OutStream anymore. @RestController works.
    Finish @GetMapping, @PostMapping, @DeletMapping, @POST(PUT)Mapping

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,39 +1,67 @@
 package com.codesoom.assignment.controllers;
 
-import org.springframework.scheduling.config.Task;
+import com.codesoom.assignment.controllers.models.Task;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    List<Task> tasks = new ArrayList<>();
+    Long id = 0L;
 
     @GetMapping("")
-    public String list() {
-        return "LIST";
+    public List<Task> lists() {
+        return tasks;
     }
 
     @GetMapping("/{id}")
-    public String lists(@PathVariable Long id) {
-        return "LISTS";
+    public Task list(@PathVariable Long id) {
+        return getTask(id);
     }
 
     @PostMapping
-    public String create(@RequestBody Task task) {
-        return "Created";
+    public Task create(@RequestBody Task task) {
+        Task newTask = new Task();
+        newTask.setTitle(task.getTitle());
+        newTask.setId(generateId());
+        tasks.add(newTask);
+
+        return newTask;
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
-        return "Deleted";
+        Task task = getTask(id);
+        tasks.remove(task);
+
+        return "";
     }
 
     @PutMapping("/{id}")
-    public String putUpdate(@PathVariable Long id, @RequestBody Task task) {
-        return "Updated";
+    public Task putUpdate(@PathVariable Long id, @RequestBody Task task) {
+        Task updateTask = getTask(id);
+        updateTask.setTitle(task.getTitle());
+        return updateTask;
     }
 
     @PatchMapping("/{id}")
     public String patchUpdate(@PathVariable Long id, @RequestBody Task task) {
+        Task updateTask = getTask(id);
+        updateTask.setTitle(task.getTitle());
         return "Updated";
+    }
+
+    public Task getTask(Long id) {
+        return tasks.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    public Long generateId(){
+        id += 1;
+        return id;
     }
 }
