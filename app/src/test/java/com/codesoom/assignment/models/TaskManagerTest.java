0c2c6288ab732755e6bac15c29c22a3cbaaaa6f8
juunini <juuni.ni.i@gmail.com>
2021-01-26 13:38:15 +0900

    Add task manager clear method structure

--- app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
@@ -4,6 +4,20 @@ import org.junit.jupiter.api.*;
 
 @DisplayName("TaskManager 클래스")
 public class TaskManagerTest {
+    @BeforeEach
+    void BeforeEach() {
+        // clear 메소드를 실행하도록 하기
+    }
+
+    @Nested
+    @DisplayName("clear 메소드는")
+    class Describe_clear {
+        @Test
+        @DisplayName("아무것도 리턴하지 않는다. clear 후에 findAll 을 하면 빈 Task 리스트를 리턴한다.")
+        void It_returns_nothing_and_find_all_returns_empty_task_list() {
+
+        }
+    }
 
     @Nested
     @DisplayName("findAll 메소드는")
@@ -114,8 +128,8 @@ public class TaskManagerTest {
         class Context_when_delete_id_is_exists {
 
             @Test
-            @DisplayName("아무것도 리턴하지 않는다.")
-            void It_returns_task() {
+            @DisplayName("아무것도 리턴하지 않는다. 식제한 Task 를 찾으려 하면 NotFoundTaskIDException 을 던진다.")
+            void It_returns_nothing_and_find_id_throws_not_found_task_id_exception() {
 
             }
         }
