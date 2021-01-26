5f0b4bf8a6e96657ea809acceaeac738e9862a1f
etff <wish2rich@gmail.com>
2021-01-26 22:32:06 +0900

    refactor: add assertion to tasks
    add @DisplayName to test

--- app/src/test/java/com/codesoom/assignment/domain/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/TaskTest.java
@@ -1,11 +1,13 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
 
+    @DisplayName("task 객체를 생성할 수 있다")
     @Test
     void canCreate() {
         Task task = new Task();
--- app/src/test/java/com/codesoom/assignment/domain/TasksTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/TasksTest.java
@@ -1,12 +1,11 @@
 package com.codesoom.assignment.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TasksTest {
     private Tasks tasks;
@@ -15,32 +14,31 @@ class TasksTest {
     @BeforeEach
     void setUp() {
         tasks = new Tasks();
-        task = new Task(1L, "test");
+        task = new Task();
     }
 
+    @DisplayName("객체를 추가했을때 컬렉션에 값이 저장된다")
     @Test
     void addTask() {
         tasks.addTask(task);
-        assertEquals(tasks.size(), 1);
+
+        Task task = tasks.findTask(1L)
+                .orElse(null);
+
+        assertEquals(1, tasks.size());
+        assertEquals(1L, task.getId());
     }
 
+    @DisplayName("객체를 제거했을때 컬렉션에 값이 없어진다.")
     @Test
     void remove() {
         tasks.addTask(task);
+
         tasks.remove(task);
-        assertEquals(tasks.size(), 0);
-    }
 
-    @Test
-    void findTask() {
-        tasks.addTask(task);
-        Optional<Task> task = tasks.findTask(1L);
-        assertEquals(task.get().getId(), 1L);
-    }
-
-    @Test
-    void getTasks() {
-        tasks.addTask(task);
-        assertNotNull(tasks.getTasks());
+        Task task = tasks.findTask(1L)
+                .orElse(null);
+        assertNull(task);
+        assertEquals(0,tasks.size());
     }
 }
