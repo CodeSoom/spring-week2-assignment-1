cb03487681593d7a36aa92af3e23a9f91a709fb5
developerOlive <chloehiho@gmail.com>
2021-01-27 05:09:46 +0900

    Refactor : id 생성 부분 수정

--- app/src/main/java/com/codesoom/assignment/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/TaskRepository.java
@@ -4,15 +4,15 @@ import com.codesoom.assignment.models.Task;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TaskRepository {
 
     public Map<Long, Task> taskStore = new HashMap<>();
-    private Long newId = 0L;
+    AtomicLong idCounter = new AtomicLong(0);
 
     public Long generateId() {
-        newId += 1;
-        return newId;
+        return idCounter.getAndIncrement();
     }
 
 }
