244e3588343c855530f88af0d73c2c1187598d71
newoo <newoo4297@naver.com>
2021-01-26 17:12:14 +0900

    Add codes about handling requests with TaskList and JsonParser

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,32 +1,42 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.Task;
+import com.codesoom.assignment.TaskList;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    TaskList taskList = new TaskList();
+    JsonParser jsonParser = new JsonParser();
+
     @GetMapping
-    public String list() {
-        return "List";
+    public String list() throws IOException {
+        return jsonParser.toJson(taskList);
     }
 
     @GetMapping("/{id:[0-9]+}")
-    public String detail() {
-        return "Detail";
+    public String detail(@PathVariable Long id) throws IOException {
+        return jsonParser.toJson(taskList.getTask(id));
     }
 
     @PostMapping
-    public String create(@RequestBody String text) {
-        return "Created";
+    public String create(@RequestBody String title) throws IOException {
+        Task task = taskList.add(title);
+        return jsonParser.toJson(task);
     }
 
     @RequestMapping(path = "/{id:[0-9]+}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public String update(@RequestBody String text) {
-        return "Updated";
+    public String update(@PathVariable Long id, @RequestBody String title) throws IOException {
+        Task task = taskList.modify(id, title);
+        return jsonParser.toJson(task);
     }
 
     @DeleteMapping("/{id:[0-9]+}")
-    public String delete() {
+    public String delete(@PathVariable Long id) throws IOException {
+        taskList.remove(id);
         return "Deleted";
     }
 }
