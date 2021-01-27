eb739964af5996433966eba9022ad865a6ad8c26
newoo <newoo4297@naver.com>
2021-01-27 19:59:23 +0900

    Add error handling at TaskController

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,9 +2,12 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.Task;
 import com.codesoom.assignment.TaskList;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 /**
  * Handle requests about task.
@@ -16,30 +19,83 @@ public class TaskController {
     JsonParser jsonParser = new JsonParser();
 
     @GetMapping
-    public String list() throws IOException {
-        return jsonParser.toJson(taskList.getTaskList());
+    public String list() {
+        try {
+            return jsonParser.toJson(taskList.getTaskList());
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Parsing error"
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
+            );
+        }
     }
 
     @GetMapping("/{id:[0-9]+}")
-    public String detail(@PathVariable Long id) throws IOException {
-        return jsonParser.toJson(taskList.getTask(id));
+    public String detail(@PathVariable Long id) {
+        try {
+            return jsonParser.toJson(taskList.getTask(id));
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not found task"
+            );
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Parsing error"
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
+            );
+        }
     }
 
     @PostMapping
-    public String create(@RequestBody String title) throws IOException {
-        Task task = taskList.add(title);
-        return jsonParser.toJson(task);
+    public String create(@RequestBody String title) {
+        try {
+            Task task = taskList.add(title);
+            return jsonParser.toJson(task);
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Parsing error"
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
+            );
+        }
     }
 
     @RequestMapping(path = "/{id:[0-9]+}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public String update(@PathVariable Long id, @RequestBody String title) throws IOException {
-        Task task = taskList.modify(id, title);
-        return jsonParser.toJson(task);
+    public String update(@PathVariable Long id, @RequestBody String title) {
+        try {
+            Task task = taskList.modify(id, title);
+            return jsonParser.toJson(task);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not found task"
+            );
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Parsing error"
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
+            );
+        }
     }
 
     @DeleteMapping("/{id:[0-9]+}")
-    public String delete(@PathVariable Long id) throws IOException {
-        taskList.remove(id);
-        return "Deleted";
+    public String delete(@PathVariable Long id) {
+        if (taskList.remove(id)) {
+            return "Deleted";
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Fail Deleted"
+            );
+        }
     }
 }
