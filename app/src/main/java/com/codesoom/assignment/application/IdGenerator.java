b509d54f34038cdfdfc69e39a015e9cfedac47ee
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-27 16:52:42 +0900

    refactor: Encapsulation field

--- app/src/main/java/com/codesoom/assignment/application/IdGenerator.java
+++ app/src/main/java/com/codesoom/assignment/application/IdGenerator.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
     private static final int START_ID = 0;
-    AtomicLong idCounter = new AtomicLong(START_ID);
+    private final AtomicLong idCounter = new AtomicLong(START_ID);
 
     public long generateNewTaskId() {
         return idCounter.getAndIncrement();
