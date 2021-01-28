fb36a2f7b6eac6ecee12bab4ac5b1fd4c4b764c2
newoo <newoo4297@naver.com>
2021-01-28 19:53:20 +0900

    Update and delete some error handlings

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -19,49 +20,26 @@ public class TaskController {
     JsonParser jsonParser = new JsonParser();
 
     @GetMapping
-    public String list() {
-        try {
-            return jsonParser.toJson(taskList.getTaskList());
-        } catch (IOException e) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Parsing error"
-            );
-        } catch (Exception e) {
-            throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
-            );
-        }
+    public List<Task> list() {
+        return taskList.getTaskList();
     }
 
     @GetMapping("/{id:[0-9]+}")
-    public String detail(@PathVariable Long id) {
+    public Task detail(@PathVariable Long id) {
         try {
-            return jsonParser.toJson(taskList.getTask(id));
+            return taskList.getTask(id);
         } catch (NoSuchElementException e) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Not found task"
-            );
-        } catch (IOException e) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Parsing error"
-            );
-        } catch (Exception e) {
-            throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
             );
         }
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String create(@RequestBody String title) {
+    public Task create(@RequestBody String title) {
         try {
-            Task task = taskList.add(title);
-            return jsonParser.toJson(task);
-        } catch (IOException e) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Parsing error"
-            );
+            return taskList.add(title);
         } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error"
