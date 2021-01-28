c8865b6cc49fd97274fbe90a01cca1a48480950b
juunini <juuni.ni.i@gmail.com>
2021-01-28 23:38:30 +0900

    Seperate component and task manager

---
+++ web/src/pages/TaskManager.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+class TaskManager {
+  async findAll() {
+    const { data } = await axios.get("http://localhost:8080/tasks");
+    return data;
+  }
+
+  async findOne(id) {
+    const { data } = await axios.get(`http://localhost:8080/tasks/${id}`);
+    return data;
+  }
+
+  async insertOne(title) {
+    const { data } = await axios.post("http://localhost:8080/tasks", {
+      title,
+    });
+    return data;
+  }
+
+  async modifyOne(id, title) {
+    const { data } = await axios.put(`http://localhost:8080/tasks/${id}`, {
+      title,
+    });
+    return data;
+  }
+
+  async deleteOne(id) {
+    const { data } = await axios.delete(`http://localhost:8080/tasks/${id}`);
+    return data;
+  }
+}
+
+module.exports = TaskManager;
---
+++ web/src/pages/component.js
@@ -0,0 +1,25 @@
+const TaskManager = require('./TaskManager');
+
+module.exports = class {
+  async onCreate() {
+    this.initializeState();
+    this.taskManager = new TaskManager();
+
+    const tasks = await this.taskManager.findAll();
+    this.setTasks(tasks);
+  }
+
+  initializeState() {
+    this.state = {
+      tasks: [],
+    };
+  }
+
+  setState(key, value) {
+    this.state[key] = value;
+  }
+
+  setTasks(tasks) {
+    this.setState("tasks", tasks);
+  }
+};
