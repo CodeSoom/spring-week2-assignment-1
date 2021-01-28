68aa31c73f9b215e2a44f1059d5b32a5e2d0d9db
Lee Dong Wook <sh95119@gmail.com>
2021-01-29 00:28:33 +0900

    FIX: include class name in toString method

--- app/src/main/java/com/codesoom/assignment/model/Task.java
+++ app/src/main/java/com/codesoom/assignment/model/Task.java
@@ -22,6 +22,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return String.format("<id: %s title: %s>", this.id, this.title);
+        return String.format("<Task: %s, id: %s title: %s>", this.id, this.title);
     }
 }
