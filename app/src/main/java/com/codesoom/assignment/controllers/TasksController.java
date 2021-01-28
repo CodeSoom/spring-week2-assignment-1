66a4d4974f18fa36dc05a059bdbefbab6e46146f
juunini <juuni.ni.i@gmail.com>
2021-01-28 12:34:25 +0900

    Set tasks uri methods

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TasksController.java
@@ -0,0 +1,42 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import com.codesoom.assignment.models.TaskManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin
+public class TasksController {
+    TaskManager taskManager = TaskManager.getInstance();
+
+    @GetMapping
+    public List<Task> findAll() {
+        return taskManager.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Task findOne(@PathVariable long id) {
+        return taskManager.findOne(id);
+    }
+
+    @PostMapping
+    public Task insertOne(@RequestBody Task task) {
+        return taskManager.insertOne(task.title());
+    }
+
+    @RequestMapping(
+            value = "{id}",
+            method = {RequestMethod.PUT, RequestMethod.PATCH}
+    )
+    public Task modifyOne(@PathVariable long id, @RequestBody Task task) {
+        return taskManager.modifyOne(id, task.title());
+    }
+
+    @DeleteMapping("{id}")
+    public Task deleteOne(@PathVariable long id) {
+        return taskManager.deleteOne(id);
+    }
+}
