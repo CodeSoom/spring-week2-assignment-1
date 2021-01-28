1fd37fbd148fe268f557ffd08b2210fac448d8c5
Reynard <gksdidrhddjq@gmail.com>
2021-01-28 22:12:34 +0900

    2020-01-28 Reynard-Hanskin project initilized and worked using Spring Framework
    added Get, Post, Put, and Delete method for making TodoList.

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,58 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin
+class TaskController {
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
+    @GetMapping()
+    public List<Task> list() {
+        return tasks;
+    }
+
+    @PostMapping()
+    public Task create(@RequestBody Task task) {
+        task.setId(generateId());
+        tasks.add(task);
+
+        return task;
+    }
+
+    @PutMapping("/{taskId}")
+    public Task update(@PathVariable long taskId, @RequestBody Task task) {
+        for(Task storedTask : tasks){
+            if(storedTask.getId() == taskId){
+                storedTask.setTitle(task.getTitle());
+                return storedTask;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<Long> delete(@PathVariable long taskId) {
+        for(Task storedTask : tasks){
+            if(storedTask.getId() == taskId){
+                tasks.remove(storedTask);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    private long generateId() {
+        newId += 1;
+        return newId;
+    }
+}
