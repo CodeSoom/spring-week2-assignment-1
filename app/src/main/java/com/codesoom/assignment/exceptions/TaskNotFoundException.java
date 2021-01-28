50d0bea01a250bf3560c4bcc380ab5c021aac089
mikekang47 <gyeong0308@gmail.com>
2021-01-28 15:57:31 +0900

    Modify variable name 'errorMessage' to 'defaultMessage'

--- app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
@@ -1,2 +1,15 @@
-package com.codesoom.assignment.exceptions;public class TaskNotFoundException {
+package com.codesoom.assignment.exceptions;
+
+public class TaskNotFoundException extends RuntimeException{
+
+    private final static String defaultMessage = "Can't find task that same as input";
+
+    public TaskNotFoundException() {
+        super(defaultMessage);
+    }
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+
 }
