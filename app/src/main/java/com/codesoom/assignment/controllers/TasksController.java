44dbe963a69cd21ca6d7bf0363e5afb7ee5f858e
juunini <juuni.ni.i@gmail.com>
2021-01-28 18:40:35 +0900

    Add task controller doc

--- app/src/main/java/com/codesoom/assignment/controllers/TasksController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TasksController.java
@@ -7,28 +7,54 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Task API.
+ */
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin
 public class TasksController {
     TaskManager taskManager = TaskManager.getInstance();
 
+    /**
+     * Returns all tasks.
+     */
     @GetMapping
     public List<Task> findAll() {
         return taskManager.findAll();
     }
 
+    /**
+     * Returns one task.
+     * If not exists, throw 404 status.
+     *
+     * @param id is target id.
+     * @return target task.
+     */
     @GetMapping("{id}")
     public Task findOne(@PathVariable long id) {
         return taskManager.findOne(id);
     }
 
+    /**
+     * Insert a task.
+     *
+     * @param task is want to insert task.
+     * @return inserted task.
+     */
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Task insertOne(@RequestBody Task task) {
         return taskManager.insertOne(task.title());
     }
 
+    /**
+     * Modify a task.
+     *
+     * @param id   is target task id.
+     * @param task is want to modify values has task.
+     * @return modified task.
+     */
     @RequestMapping(
             value = "{id}",
             method = {RequestMethod.PUT, RequestMethod.PATCH}
@@ -37,6 +63,12 @@ public class TasksController {
         return taskManager.modifyOne(id, task.title());
     }
 
+    /**
+     * Delete a task.
+     *
+     * @param id is target id.
+     * @return deleted task.
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public Task deleteOne(@PathVariable long id) {
