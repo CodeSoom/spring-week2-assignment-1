72aeeba3416db29c2c6863f5d328ead768b3cc5e
mikekang47 <gyeong0308@gmail.com>
2021-01-28 15:36:18 +0900

    Modify Task.java Make update method instead of setTitle for PUT and PATCH

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -4,12 +4,22 @@ public class Task {
     private Long id;
     private String title;
 
+    public Task(String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Task(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void update(Task task) {
+        this.title = task.getTitle();
     }
 
     public Long getId() {
