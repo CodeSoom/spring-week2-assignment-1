4f261bac1b19eeae2608b3eeed59c340a6db156e
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-28 13:56:07 +0900

    Remove empty line in ErrorMessage

--- app/src/main/java/com/codesoom/assignment/ErrorMessage.java
+++ app/src/main/java/com/codesoom/assignment/ErrorMessage.java
@@ -5,11 +5,8 @@ import java.util.Date;
 public class ErrorMessage {
 
     private int statusCode;
-
     private Date timestamp;
-
     private String message;
-
     private String description;
 
     public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
