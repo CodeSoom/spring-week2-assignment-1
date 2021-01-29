291050352326f35bd275227640eac555f3d2176a
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-29 19:04:10 +0900

    Rename ErrorMessage to ErrorResponse

--- app/src/main/java/com/codesoom/assignment/ErrorResponse.java
+++ app/src/main/java/com/codesoom/assignment/ErrorResponse.java
@@ -2,14 +2,14 @@ package com.codesoom.assignment;
 
 import java.util.Date;
 
-public class ErrorMessage {
+public class ErrorResponse {
 
     private int statusCode;
     private Date timestamp;
     private String message;
     private String description;
 
-    public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
+    public ErrorResponse(int statusCode, Date timestamp, String message, String description) {
         this.statusCode = statusCode;
         this.timestamp = timestamp;
         this.message = message;
--- app/src/main/java/com/codesoom/assignment/TaskAdvice.java
+++ app/src/main/java/com/codesoom/assignment/TaskAdvice.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.http.HttpResponse;
 import java.util.Date;
 
 @RestControllerAdvice
@@ -17,24 +18,24 @@ public class TaskAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<Object> handleNotFound(TaskNotFoundException exception, WebRequest request) {
-        ErrorMessage errorMessage = new ErrorMessage(
+        ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 new Date(),
                 exception.getMessage(),
                 request.getDescription(false));
 
-        return new ResponseEntity(errorMessage, HttpStatus.NOT_FOUND);
+        return new ResponseEntity(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorMessage errorMessage = new ErrorMessage(
+        ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
 
-        return new ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
