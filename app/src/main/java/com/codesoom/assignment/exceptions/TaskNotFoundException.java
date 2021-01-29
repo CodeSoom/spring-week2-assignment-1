f83729293f172191ac9e30946ad754d96cc0e79a
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-30 08:58:52 +0900

    Remove a constant message and Put the message in the super method

--- app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
@@ -1,11 +1,9 @@
 package com.codesoom.assignment.exceptions;
 
-public class TaskNotFoundException extends RuntimeException{
-
-    private final static String defaultMessage = "task가 존재하지 않습니다.";
+public class TaskNotFoundException extends RuntimeException {
 
     public TaskNotFoundException() {
-        super(defaultMessage);
+        super("task가 존재하지 않습니다.");
     }
 
     public TaskNotFoundException(String message) {
