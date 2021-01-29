3d464fa6836c70adec70b6b2367f5a436de71ab3
etff <wish2rich@gmail.com>
2021-01-30 01:20:06 +0900

    doc: add task javadoc

--- app/src/main/java/com/codesoom/assignment/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -2,6 +2,13 @@ package com.codesoom.assignment.domain;
 
 import java.util.Objects;
 
+/**
+ * 할 일을 나타내는 클래스.
+ *
+ * @author etff
+ * @version 1.0.0 21/01/30
+ * @see com.codesoom.assignment.domain.Task
+ */
 public class Task {
     private Long id;
 
@@ -15,18 +22,34 @@ public class Task {
         this.title = title;
     }
 
+    /**
+     * 할 일의 아이디를 돌려준다.
+     * @return 할 일의 아이디
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * 할 일의 아이디 값을 지정한다.
+     * @param id 할일의 해당하는 아이디
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * 어떤 할 일인지 돌려준다.
+     * @return 해야할 할 일 명칭
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * 해야할 할 일을 지정한다.
+     * @param title 해야할 할 일 명칭
+     */
     public void setTitle(String title) {
         this.title = title;
     }
