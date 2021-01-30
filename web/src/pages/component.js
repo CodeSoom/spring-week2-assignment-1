c3fe4ca0c857476fa274fed44a294768a86a3360
juunini <juuni.ni.i@gmail.com>
2021-01-30 12:27:25 +0900

    Add handle change title

--- web/src/pages/component.js
+++ web/src/pages/component.js
@@ -9,14 +9,33 @@ module.exports = class {
     this.setTasks(tasks);
   }
 
+  handleChangeTitle() {
+    const titleSelector = this.getTitleSelector();
+    const title = titleSelector.value;
+    this.setTitle(title);
+  }
+
+  handleClickAddButton() {
+
+  }
+
   initializeState() {
     this.state = {
       tasks: [],
+      title: '',
     };
+  }
+
+  getTitleSelector() {
+    return document.getElementById("task")
   }
 
   setState(key, value) {
     this.state[key] = value;
+  }
+
+  setTitle(title) {
+    this.setState('title', title);
   }
 
   setTasks(tasks) {
