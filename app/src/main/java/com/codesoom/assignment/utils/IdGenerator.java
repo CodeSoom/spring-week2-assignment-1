482eea66b73e64c50344590064d8c6ee96ca3637
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-30 10:58:11 +0900

    Add IdGenerator JavaDoc

--- app/src/main/java/com/codesoom/assignment/utils/IdGenerator.java
+++ app/src/main/java/com/codesoom/assignment/utils/IdGenerator.java
@@ -4,11 +4,19 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * id를 생성하기 위한 클래스
+ */
 @Component
 public class IdGenerator {
 
     private AtomicLong newId = new AtomicLong(0);
 
+    /**
+     * id를 생성해 리턴합니다.
+     *
+     * @return 생성된 id
+     */
     public Long generateId() {
         return newId.incrementAndGet();
     }
