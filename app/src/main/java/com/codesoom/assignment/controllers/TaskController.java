e171705479d1695f127b20b9516c46be89e7ee83
Lee Dong Wook <sh95119@gmail.com>
2021-01-31 23:50:12 +0900

    REFACTOR: add curly brackets of if statement

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.controllers;
 import java.util.List;
 
 import com.codesoom.assignment.dto.DeleteResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
--- app/src/main/java/com/codesoom/assignment/model/Task.java
+++ app/src/main/java/com/codesoom/assignment/model/Task.java
@@ -35,8 +35,12 @@ public class Task {
     @Override
     public boolean equals(Object obj) {
 
-        if (obj == this) return true;
-        if (!(obj instanceof Task)) return false;
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Task)) {
+            return false;
+        }
 
         Task task = (Task) obj;
         return this.id == task.id &&
