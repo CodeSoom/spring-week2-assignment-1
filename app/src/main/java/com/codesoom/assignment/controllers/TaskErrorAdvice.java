bd9974cb190f37eca44917d3f9cee22d308585c9
mikekang47 <gyeong0308@gmail.com>
2021-01-31 16:41:52 +0900

    Add ErrorResponse.java to give send a data Plus send a error sentece when call ErrorResponse

--- app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.exceptions.TaskNotFoundException;
+import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +13,8 @@ public class TaskErrorAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(TaskNotFoundException.class)
-    public String handleNOtFound() {
-        return "Can't find task that same as input";
+    public ErrorResponse handleNOtFound() {
+        return new ErrorResponse("Can't find task that same as input");
 
     }
 }
