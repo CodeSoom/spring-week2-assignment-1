fbcdb1d4ef5517acf383c81e7c4f63b14f29bd65
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-05-10 18:06:30 +0900

    REST API 작성

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,31 +2,35 @@ package com.codesoom.assignment.controllers;
 // 1. READ Collection - GET / tasks => 완료
 // 2. READ Item - GET / tasks/{id} => 완료
 // 3. CREATE POST/tasks => 완료
-// 4. UPDATE PUT/PATCH/tasks/{id} =>
-// 5. DELETE DELETE/tasks/{id}
+// 4. UPDATE PUT/PATCH/tasks/{id} => 완료
+// 5. DELETE DELETE/tasks/{id} => 완료
 
 import com.codesoom.assignment.models.Task;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
     private List<Task> tasks = new ArrayList<>();
     private Long newId = 0L;
 
+
+    // GET
     @GetMapping
     public List<Task> list() {
         return tasks;
     }
 
+    // GET tasks/id
     @GetMapping("/{id}")
     public Task getTask(@PathVariable(value = "id") Long id) {
-        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+        return selectTask(tasks, id);
     }
 
+    // POST
     @PostMapping
     public Task create(@RequestBody Task task) {
         task.setId(generateId());
@@ -34,14 +38,25 @@ public class TaskController {
         return task;
     }
 
+    // UPDATE
     @PutMapping("/{id}")
     public Task update(@PathVariable(value = "id") Long id, @RequestBody Task task) {
-        Task originTask = tasks.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
+        Task originTask = selectTask(tasks, id);
         if (originTask != null) {
             originTask.setTitle(task.getTitle());
             return originTask;
         }
         return null;
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(value = "id") Long id){
+        tasks.remove(selectTask(tasks, id));
+    }
+
+    private Task selectTask(List<Task> tasks, Long id) {
+        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
     }
 
     private Long generateId(){
