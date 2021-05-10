262902c61c4a82ef0df3cdefd5870e069c895b2b
Changsu Im <imcxsu@gmail.com>
2021-05-10 23:54:08 +0900

    Add methods to get, put, delete task by id

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +23,22 @@ import java.util.Map;
 @RequestMapping("/tasks")
 public class TaskController {
     private final Logger logger = LoggerFactory.getLogger(TaskController.class);
-    private Map<Long, Task> tasks = new HashMap();
+    private final Map<Long, Task> tasks = new HashMap<>();
     private Long newTaskId = 1L;
 
     @GetMapping
-    public ResponseEntity<List<Task>> get() {
-        var tasksList = new ArrayList<>(tasks.values());
+    public ResponseEntity<List<Task>> list() {
+        final var tasksList = new ArrayList<>(tasks.values());
         return ResponseEntity.ok(tasksList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> get(@PathVariable("id") final Long id) {
+        final var task = tasks.get(id);
+        if (task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
     }
 
     @PostMapping
@@ -38,5 +50,27 @@ public class TaskController {
         task.setId(newTaskId++);
         tasks.put(task.getId(), task);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@PathVariable("id") final Long id, @RequestBody final Task newTask) {
+        if (id == null || newTask.getTitle().isBlank()) {
+            logger.debug("id={}, task={}", id, newTask);
+            return ResponseEntity.badRequest().build();
+        }
+        var oldTask = tasks.get(id);
+        oldTask.setTitle(newTask.getTitle());
+        return ResponseEntity.ok().build();
+    }
+
+//    @PatchMapping("/{id}")
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") final Long id) {
+        final var deletedTask = tasks.remove(id);
+        if (deletedTask == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 }
