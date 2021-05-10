90f07a5473568aba958803ff6e01e26ca98860d8
sedin <sedin@kakao.com>
2021-05-10 23:56:56 +0900

    feat: GET, PUT, DELETE api 로직 추가
    - Get: get request task
    - Put: modify request task
    - Delete: delete request task
    - 테스트를 통과 하도록 로직 구현

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,10 +1,13 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -15,16 +18,51 @@ public class TaskController {
     private Long newTaskId = 0L;
 
     @GetMapping("")
-    public List<Task> getTaskList() {
-        return taskList;
+    public ResponseEntity getTaskList() {
+        return new ResponseEntity(taskList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getTask(@PathVariable Long id) {
+        Optional<Task> searchTask = taskList.stream()
+                                            .filter(task -> task.getId().equals(id))
+                                            .findFirst();
+        if (!searchTask.isPresent()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(searchTask.get(), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public Task createTask(@RequestBody Task task) {
+    public ResponseEntity createTask(@RequestBody Task task) {
         task.setId(generateId());
         taskList.add(task);
 
-        return task;
+        return new ResponseEntity(task, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateTask(@PathVariable Long id, @RequestBody Task requestTask) {
+        Optional<Task> searchTask = taskList.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst();
+        if (!searchTask.isPresent()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        searchTask.get().setTitle(requestTask.getTitle());
+        return new ResponseEntity(searchTask.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteTask(@PathVariable Long id) {
+        Optional<Task> searchTask = taskList.stream()
+                                            .filter(task -> task.getId().equals(id))
+                                            .findFirst();
+        if (!searchTask.isPresent()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        taskList.remove(searchTask.get());
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     private Long generateId() {
