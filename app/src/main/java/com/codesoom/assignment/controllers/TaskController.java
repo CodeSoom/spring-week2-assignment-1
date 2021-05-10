a2d03f13bd993002ce04ab10515ebc10b2c48c4a
Changsu Im <imcxsu@gmail.com>
2021-05-10 22:59:38 +0900

    Add get, post tasks mapping to TaskController

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,40 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private Map<Long, Task> tasks = new HashMap();
+    private Long newTaskId = 1L;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> get() {
+        var tasksList = new ArrayList<>(tasks.values());
+        return ResponseEntity.ok(tasksList);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> create(@RequestBody Task task) {
+        if (task.getTitle().isBlank()) {
+            return ResponseEntity.badRequest().build();
+        }
+        task.setId(newTaskId++);
+        tasks.put(task.getId(), task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+}
---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.models;
+
+/**
+ * 할 일.
+ */
+public class Task {
+    private Long id;
+    private String title;
+
+    public Task(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Task{id=%s, title=%s}", id, title);
+    }
+}
