75b8139cbd6c67803d8dbd47afb0be77ff3f9794
Changsu Im <imcxsu@gmail.com>
2021-05-11 20:45:32 +0900

    Remove unused import

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -52,7 +51,6 @@ public class TaskController {
         tasks.put(task.getId(), task);
         return new ResponseEntity<>(task, HttpStatus.CREATED);
     }
-
 
     @PutMapping("/{id}")
     public ResponseEntity<Task> update(@PathVariable("id") final Long id, @RequestBody final Task newTask) {
