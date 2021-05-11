1c046eab0d93299eeaf3423d2b644ee5421bf0b8
humblEgo <injun.woo30000@gmail.com>
2021-05-11 12:42:19 +0900

    feat: task model

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+
+    private String title;
+    private Long id = 0L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
