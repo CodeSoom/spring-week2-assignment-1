b1614d53e1549610ddda532bea21677240e6c97b
humblEgo <injun.woo30000@gmail.com>
2021-05-11 12:42:39 +0900

    feat: /tasks GET, POST 구현

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,16 +1,18 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.models.Task;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 // TODO
 // 1. Read Collection - GET /tasks
@@ -24,14 +26,19 @@ import java.util.ArrayList;
 @RequestMapping("/tasks")
 public class TaskController {
 
+    private List<Task> tasks = new ArrayList<>();
+    private Long currentTaskId = 0L;
+
     @GetMapping
-    public String getTaskList() {
-        return "Task list";
+    public List<Task> getTaskList() {
+        return tasks;
     }
 
     @PostMapping
-    public String addTask() {
-        return "created";
+    public Task addTask(@RequestBody Task task) {
+        task.setId(generateId());
+        tasks.add(task);
+        return task;
     }
 
     @GetMapping("/{id}")
@@ -54,4 +61,7 @@ public class TaskController {
         return "task id: " + id + " deleted!";
     }
 
+    private synchronized long generateId() {
+        return this.currentTaskId += 1;
+    }
 }
