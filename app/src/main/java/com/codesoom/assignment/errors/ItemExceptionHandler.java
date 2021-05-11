f2e885550cd7daa6f80b6aafcd212f7de33512e6
Joowan Kim <rlawndhks217@gmail.com>
2021-05-12 01:13:07 +0900

    Add ItemExceptionHandler
    I changed ExceptionHandler name to ItemExceptionHandler

--- app/src/main/java/com/codesoom/assignment/errors/ItemExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/errors/ItemExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class ItemExceptionHandler {
     @ExceptionHandler(ItemNotFoundException.class)
     public ResponseEntity handleItemNotFoundException(ItemNotFoundException e) {
         return new ResponseEntity(HttpStatus.NOT_FOUND);
