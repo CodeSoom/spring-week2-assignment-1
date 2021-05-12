9d532678efb0162e812a1fe9bfb8781b2fa76bcf
Changsu Im <imcxsu@gmail.com>
2021-05-13 01:12:50 +0900

    Remove unused logger from controller advice

--- app/src/main/java/com/codesoom/assignment/controllers/ExceptionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ExceptionController.java
@@ -2,8 +2,6 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.exceptions.TaskNotFoundException;
 import com.codesoom.assignment.exceptions.TaskTitleEmptyException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,23 +10,18 @@ import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class ExceptionController {
-    private final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
-
     @ExceptionHandler({TaskTitleEmptyException.class})
     public ResponseEntity<String> taskTitleMustNotBeBlank(final RuntimeException ex) {
-        logger.warn("error", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({TaskNotFoundException.class})
     public ResponseEntity<String> taskNotFound(final RuntimeException ex) {
-        logger.warn("error", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({HttpServerErrorException.InternalServerError.class})
     public ResponseEntity<String> internalServerError(final RuntimeException ex) {
-        logger.warn("error", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
