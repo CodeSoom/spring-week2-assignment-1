82011af3abc78300ad8a94617398a67cd769fe69
DevRunner21 <devrunner21@gmail.com>
2021-05-12 14:17:41 +0900

    TaskService 작성 및 로깅 처리
    - Task데이터가 담겨있는 Map을 Service Layer로 이관
    - Controller, Service에 JavaDoc 주석 작성
    - Request, Response 로그 작성
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,71 +7,123 @@
 
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.common.exceptions.NotFoundTaskException;
 import com.codesoom.assignment.models.Task;
+import com.codesoom.assignment.services.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
+/**
+ * 할 일(Task) 관련 Request를 수신하고 Response를 반환합니다.
+ */
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin
 public class TaskController {
 
-    private Long taskIdSeq = 0L;
-    private LinkedHashMap<Long, Task> tasksMap = new LinkedHashMap<>();
+    @Autowired
+    private TaskService taskService;
 
+    private Logger log = LoggerFactory.getLogger(TaskController.class);
+
+    /**
+     * 할 일 리스트를 반환합니다.
+     * @return 할 일 리스트
+     */
     @GetMapping
     public List<Task> list() {
 
-        List<Task> taskList = tasksMap.values().stream()
-                .collect(Collectors.toList());
+        log.info(">>>>>>>>>>> Access Controller 할 일 목록 조회");
+        List<Task> taskList = this.taskService.getTaskList();
+
+        log.info(">>>>>>>>>>> Response : " + taskList.toString());
 
         return taskList;
 
     }
 
+    /**
+     * 주어진 할 일 ID에 해당하는 할 일 정보를 조회하여 반환합니다.
+     * @param taskId 조회할 할 일 ID
+     * @return 조회한 할 일
+     */
     @GetMapping("{taskId}")
     public Task detail(@PathVariable("taskId") Long taskId) {
 
-        Task findTask = tasksMap.get(taskId);
+        log.info(">>> Access Controller 할 일 상세조회");
+        log.info("==== Req Param ====");
+        log.info("taskId : " + taskId);
+        log.info("===================");
 
-        if( findTask == null ) {
-            throw new NotFoundTaskException(taskId);
-        }
+        Task findTask = this.taskService.getTaskOne(taskId);
 
+        log.info(">>>>>>>>>>> Response : " + findTask.toString());
+        
         return findTask;
-
     }
 
+    /**
+     * 새로운 할 일을 등록합니다.
+     * @param newTask 새로 등록할 할 일
+     * @return 새로 등록한 할 일
+     */
     @PostMapping
     public Task create(@RequestBody Task newTask) {
 
-        newTask.setId(taskIdSeq++);
-        tasksMap.put(newTask.getId(), newTask);
+        log.info(">>>>>>>>>>> Access Controller 할 일 등록");
+        log.info("==== Req Param ====");
+        log.info("newTask : " + newTask.toString());
+        log.info("===================");
+        Task createdTask = this.taskService.saveTask(newTask);
 
-        return newTask;
+        log.info(">>>>>>>>>>>> Response : " + createdTask.toString());
+
+        return createdTask;
 
     }
 
-    @PutMapping
+    /**
+     * 주어진 할 일 Id에 해당하는 할 일의 제목을 수정합니다.
+     * @param taskId 수정 대상인 할 일의 Id
+     * @param task 수정할 할 일 정보
+     * @return 수정한 할 일
+     */
+    @PutMapping("{taskId}")
     @PatchMapping("{taskId}")
     public Task update(@PathVariable("taskId") Long taskId, @RequestBody Task task) {
 
-        Task findTask = tasksMap.get(taskId);
-        findTask.setTitle(task.getTitle());
+        log.info(">>>>>>>>>>> Access Controller 할 일 수정");
+        log.info("==== Req Param ====");
+        log.info("taskId : " + taskId);
+        log.info("task : " + task.toString());
+        log.info("===================");
 
-        return findTask;
+        Task updatedTask = this.taskService.updateTask(taskId, task.getTitle());
 
+        log.info(">>>>>>>>>>> Response : " + updatedTask.toString());
+
+        return updatedTask;
     }
 
+    /**
+     * 주어진 할 일 Id에 해당하는 할 일을 목록에서 삭제합니다.
+     * @param taskId 삭제할 할 일의 Id
+     * @return 삭제한 할 일
+     */
     @DeleteMapping("{taskId}")
     public Task delete(@PathVariable("taskId") Long taskId) {
 
-        Task findTask = tasksMap.remove(taskId);
+        log.info(">>>>>>>>>>> Access Controller 할 일 삭제");
+        log.info("==== Req Param ====");
+        log.info("taskId : " + taskId);
+        log.info("===================");
+        Task deletedTask = this.taskService.removeTask(taskId);
 
-        return findTask;
+        log.info(">>>>>>>>>>> Response : " + deletedTask.toString());
+
+        return deletedTask;
 
     }
 
--- app/src/main/java/com/codesoom/assignment/services/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/services/TaskService.java
@@ -1,7 +1,120 @@
 package com.codesoom.assignment.services;
 
+import com.codesoom.assignment.common.exceptions.NotFoundTaskException;
+import com.codesoom.assignment.controllers.TaskController;
+import com.codesoom.assignment.models.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 할 일(Task) 데이터를 가공하여 반환합니다.
+ */
 @Service
 public class TaskService {
+
+    private Long taskIdSeq = 0L;
+    private LinkedHashMap<Long, Task> tasksMap = new LinkedHashMap<>();
+
+    private Logger log = LoggerFactory.getLogger(TaskService.class);
+
+    /**
+     * 할 일 리스트를 반환합니다.
+     * @return 할 일 리스트
+     */
+    public List<Task> getTaskList() {
+
+        log.info(">>> Access Service 할 일 목록 조회");
+
+        List<Task> taskList = tasksMap.values().stream()
+                .collect(Collectors.toList());
+
+        return taskList;
+
+    }
+
+    /**
+     * 주어진 할 일 ID에 해당하는 할 일 정보를 조회하여 반환합니다.
+     * @param taskId 조회할 할 일 ID
+     * @return 조회한 할 일 정보
+     */
+    public Task getTaskOne(Long taskId) {
+
+        log.info(">>> Access Service 할 일 상세조회");
+
+        Task findTask = tasksMap.get(taskId);
+
+        if( findTask == null ) {
+            throw new NotFoundTaskException(taskId);
+        }
+
+        return findTask;
+
+    }
+
+    /**
+     * 새로운 할 일을 등록합니다.
+     * @param newTask 새로 등록할 할 일
+     * @return 등록한 할 일
+     */
+    public Task saveTask(Task newTask) {
+
+        log.info(">>> Access Service 할 일 등록");
+
+        newTask.setId(taskIdSeq++);
+        tasksMap.put(newTask.getId(), newTask);
+
+        return newTask;
+
+    }
+
+    /**
+     * 주어진 할 일 Id에 해당하는 할 일의 제목을 수정합니다.
+     * @param taskId 수정 대상인 할 일의 Id
+     * @param newTitle 수정할 할 일 제목
+     * @return 수정한 할 일
+     */
+    public Task updateTask(Long taskId, String newTitle) {
+
+        log.info(">>> Access Service 할 일 수정");
+
+        Task findTask = tasksMap.get(taskId);
+
+        // 해당하는 Task가 없을 경우 예외 발생
+        if(findTask == null){
+            throw new NotFoundTaskException(taskId);
+        }
+
+        findTask.setTitle(newTitle);
+
+        return findTask;
+
+    }
+
+    /**
+     * 주어진 할 일 Id에 해당하는 할 일을 목록에서 삭제합니다.
+     * @param taskId 삭제할 할 일의 Id
+     * @return 삭제한 할 일
+     */
+    public Task removeTask(Long taskId) {
+
+        log.info(">>> Access Service 할 일 삭제");
+
+        Task findTask = tasksMap.get(taskId);// taskId에 해당하는 Task를 구함
+
+        // 해당하는 Task가 없을 경우 예외 발생
+        if(findTask == null){
+            throw new NotFoundTaskException(taskId);
+        }
+
+        tasksMap.remove(taskId);
+
+        return findTask;
+
+    }
+
 }
