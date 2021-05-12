399547539e2e7af22bdc356321e1e5eccfe6c0fd
Joowan Kim <rlawndhks217@gmail.com>
2021-05-12 23:36:09 +0900

    Edit the comment of TaskController

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * It provides to map requests to proper method of TaskRepository
+ * A Controller map requests to proper method of Repository
  */
 @RestController
 @RequestMapping(path = "/tasks")
