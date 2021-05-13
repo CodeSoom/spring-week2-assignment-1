a8140813c333a41067e5f8ba7e65ddfbe43e2982
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-05-13 13:00:41 +0900

    테스트 코드 통과하도록 예외처리 작성

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,13 +5,16 @@ package com.codesoom.assignment.controllers;
 // 4. UPDATE PUT/PATCH/tasks/{id} => 완료
 // 5. DELETE DELETE/tasks/{id} => 완료
 
+import com.codesoom.assignment.ErrorCreate;
 import com.codesoom.assignment.models.Task;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
 
     private List<Task> tasks = new ArrayList<>();
@@ -32,6 +35,7 @@ public class TaskController {
 
     // POST
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task) {
         task.setId(generateId());
         tasks.add(task);
@@ -51,16 +55,34 @@ public class TaskController {
 
     // DELETE
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable(value = "id") Long id){
         tasks.remove(selectTask(tasks, id));
     }
 
     private Task selectTask(List<Task> tasks, Long id) {
-        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+//        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElseThrow(ErrorCreate::new);
     }
 
     private Long generateId(){
         newId += 1;
         return newId;
     }
+
+//    public static class StatusCode {
+//        public static final int OK = 200;
+//        public static final int CREATED = 201;
+//        public static final int NO_CONTENT = 204;
+//        public static final int BAD_REQUEST =  400;
+//        public static final int UNAUTHORIZED = 401;
+//        public static final int FORBIDDEN = 403;
+//        public static final int NOT_FOUND = 404;
+//        public static final int INTERNAL_SERVER_ERROR = 500;
+//        public static final int SERVICE_UNAVAILABLE = 503;
+//        public static final int DB_ERROR = 600;
+//    }
+
 }
+
+// status code를 어디로 리턴해야 하는 것인지.
---
+++ memo.md
@@ -0,0 +1,8 @@
+spring dev tools dependency 추가할 때
+- 괄호 없이 작성 시, 큰따옴표(" ") 대신 작은 따옴표 사용해야 함(' ')
+
+build와 서버 구동의 명확한 차이
+
+marko대신 react를 연동할 순 없을까?
+
+- ErrorCreate 클래스에서 BAD_REQUEST 대신, NOT_FOUND로 수정 -> 둘의 차이점 파악할 것
