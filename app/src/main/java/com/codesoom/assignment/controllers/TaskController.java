645bf14c6ba18f6320d9012d054c4289207f02de
sedin <sedin@kakao.com>
2021-05-14 22:08:58 +0900

    refactor: synchronized 키워드 추가
    - 멀티 쓰레드 상황을 고려해  synchronized 추가
    - import * 제거 후 다시 import 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,7 +3,15 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.models.Task;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,7 +73,7 @@ public class TaskController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    private Long generateId() {
+    private synchronized Long generateId() {
         newTaskId = newTaskId + 1;
         return newTaskId;
     }
