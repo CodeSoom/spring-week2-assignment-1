1bcc0f81d92087e21db285cd36d4b4d02966d792
yeonhwa <yeonhwa6717@gmail.com>
2021-05-15 11:02:47 +0900

    Tasks를 읽어와라
    Task model을 추가하라
    list 객체를 추가하라.
    list 객체에서 tasks를 리턴하라.
    @GetMapping를 추가하라.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,12 +1,20 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.models.Task;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
-    @RequestMapping("/tasks")
-    public String task() {
-        return "Task";
+    private List<Task> tasks = new ArrayList<>();
+
+    @GetMapping
+    public List<Task> list() {
+        return tasks;
     }
 }
