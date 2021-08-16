43699b57fb474cac5b31757341c5683a20927ff8
NohJangHyuk <nohriter@gmail.com>
2021-08-16 17:15:23 +0900

    Change tasks type

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -6,65 +6,73 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    private final List<Task> tasks = new ArrayList<>();
+    private final List<Map<String, Task>> tasks = new ArrayList<>();
+    private final Map<String, Task> taskMap = new HashMap<>();
     private Long newId = 0L;
 
     @GetMapping
-    public List<Task> getTasks(){
+    public List<Map<String, Task>> getTasks() {
         return tasks;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTask(@PathVariable("id") Long id){
-        Task task = findTask(id);
-        if(task == null){
+    public ResponseEntity<Task> getTask(@PathVariable("id") Long id) {
+        Optional<Task> task = findTask(id);
+        if (task.isEmpty()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity(task, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task){
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
         task.setId(generateId());
-        tasks.add(task);
+        taskMap.put(task.getId() + "", task);
+        if (tasks.isEmpty()) {
+            tasks.add(taskMap);
+        }
 
         return new ResponseEntity(task, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Task> updateTaskByPatch(@PathVariable("id") Long id, @RequestBody Task task){
-        Task findedtask = findTask(id);
-        if(findedtask == null){
+    public ResponseEntity<Task> updateTaskByPatch(@PathVariable("id") Long id, @RequestBody Task task) {
+        Optional<Task> findtask = findTask(id);
+        if (findtask.isEmpty()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        findedtask.setTitle(task.getTitle());
+        findtask.get().setTitle(task.getTitle());
 
-        return new ResponseEntity(findedtask, HttpStatus.OK);
+        return new ResponseEntity(findtask, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTaskByPUT(@PathVariable("id") Long id, @RequestBody Task task){
-        Task findedtask = findTask(id);
-        if(findedtask == null){
+    public ResponseEntity<Task> updateTaskByPUT(@PathVariable("id") Long id, @RequestBody Task task) {
+        Optional<Task> findtask = findTask(id);
+        if (findtask.isEmpty()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        findedtask.setTitle(task.getTitle());
+        findtask.get().setTitle(task.getTitle());
 
-        return new ResponseEntity(findedtask, HttpStatus.OK);
+        return new ResponseEntity(findtask, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteTask(@PathVariable("id") Long id){
-        Task findedtask = findTask(id);
-        if(findedtask == null){
+    public ResponseEntity deleteTask(@PathVariable("id") Long id) {
+        Optional<Task> findtask = findTask(id);
+        if (findtask.isEmpty()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        tasks.remove(findedtask);
+        taskMap.remove(String.valueOf(id));
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
@@ -73,10 +81,13 @@ public class TaskController {
         return newId;
     }
 
-    private Task findTask(Long id){
-        return tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    private Optional<Task> findTask(Long id) {
+        Optional<Task> task = Optional.empty();
+        Task findTask = taskMap.get(String.valueOf(id));
+
+        if (findTask == null) {
+            return task;
+        }
+        return task.of(findTask);
     }
 }
