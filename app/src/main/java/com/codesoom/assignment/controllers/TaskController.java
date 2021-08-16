3743ae02220ce813dc881c79b05efcf5ab4dd16c
Sungeheon <elon4856@gmail.com>
2021-08-16 15:54:56 +0900

    feat: post task를 구현하세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tasks")
 public class TaskController {
   private List<Task> tasks = new ArrayList<>();
+  Long id =0L;
 
   @GetMapping
   public List<Task> taskList() {
@@ -21,6 +24,18 @@ public class TaskController {
   @GetMapping("/{id}")
   public Task targetTask(@PathVariable("id") Long id) {
     return findTask(id);
+  }
+
+  @PostMapping
+  public Task create(@RequestBody Task task) throws Throwable {
+    task.setId(generateId());
+    tasks.add(task);
+    return task;
+  }
+
+  private Long generateId() {
+    id++;
+    return id;
   }
 
   private Task findTask(Long id) {
