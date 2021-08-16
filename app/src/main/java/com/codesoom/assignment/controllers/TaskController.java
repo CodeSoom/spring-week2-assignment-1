fde6fd04ff23f8f6f135dd51f2eee35ec10cd890
injun <ison@sfu.ca>
2021-08-16 23:52:36 +0900

    feat: TaskController 생성

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,12 +1,12 @@
 package com.codesoom.assignment.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @RequestMapping("/")
-    public String sayHello() {
-        return "Hello, world!!";
-    }
+@RequestMapping("/tasks")
+@CrossOrigin
+public class TaskController {
+    
 }
