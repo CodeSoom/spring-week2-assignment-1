48b013d3de957d98c74b26fc746ec5e8732ccf1d
bloomspes <bloomspes@gmail.com>
2021-08-16 18:42:31 +0900

    TaskController 의 GetMapping 구현 - 매핑할 때, public 선언시 Array 객체를 할당하는 경우, json으로 자동으로 바뀜 - Spring Web이 Array 객체를 알아서 json 형태로 변환해줌. (jackson을 사용하고 있음)

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,19 +7,26 @@
 
 package com.codesoom.assignment.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public String list() {
-        return "LIST";
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
+    // @RequestMapping(path = "", method = RequestMethod.GET)
+    @GetMapping
+    public List<Task> list() {
+        return tasks;
     }
 
-    @RequestMapping(path = "", method = RequestMethod.POST)
+    // @RequestMapping(path = "", method = RequestMethod.POST)
+    @PostMapping
     public String create() {
         return "Created";
     }
