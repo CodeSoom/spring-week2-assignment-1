802cbae94592e06092df1f50fa0885ddbfd02208
Sungeheon <elon4856@gmail.com>
2021-08-16 19:40:56 +0900

    feat: STATUS CODE를 설정하세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.models.Task;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,16 +21,19 @@ public class TaskController {
   Long id =0L;
 
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public List<Task> taskList() {
     return tasks;
   }
   
   @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
   public Task targetTask(@PathVariable("id") Long id) {
     return findTask(id);
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Task create(@RequestBody Task task) throws Throwable {
     task.setId(generateId());
     tasks.add(task);
@@ -37,6 +42,7 @@ public class TaskController {
 
 
   @RequestMapping(path="/{id}", method={RequestMethod.PATCH, RequestMethod.PUT})
+  @ResponseStatus(HttpStatus.OK)
   public Task rewrite(@RequestBody Task rewrittenTask,@PathVariable("id") Long id) throws Throwable {
     Task targetTask = findTask(id);
     targetTask.setTitle(rewrittenTask.getTitle());
@@ -44,6 +50,7 @@ public class TaskController {
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public List<Task> delete(@PathVariable("id") Long id){
     tasks.remove(findTask(id));
     return tasks;
