9e38c5f74b2d590e048831b752065bc66766ddb9
Sungeheon <elon4856@gmail.com>
2021-08-16 15:34:36 +0900

    feat: get targetTask를 구현하세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.models.Task;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,16 @@ public class TaskController {
   @GetMapping
   public List<Task> taskList() {
     return tasks;
+  }
+  
+  @GetMapping("/{id}")
+  public Task targetTask(@PathVariable("id") Long id) {
+    return findTask(id);
+  }
 
+  private Task findTask(Long id) {
+    return tasks.stream()
+        .filter(task -> task.getId().equals(id))
+        .findFirst().orElse(null);
   }
 }
