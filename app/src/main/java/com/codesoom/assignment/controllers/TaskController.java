c3543559ea9f1662ad6775b12c78d8a453b89e5a
Sungeheon <elon4856@gmail.com>
2021-08-17 19:29:07 +0900

    fix: delete와 get에 없는 id로 요청시 404에러를 return하도록 하세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,11 +1,3 @@
-//todo
-// 2. @ExceptionHandler가 Exception.class를 갖는 건 처리 범위가 필요 이상으로 커 보이네요. 좀 더 구체화된 예외를 정의하고, 새로 정의한 예외를 사용해 보세요.
-// 3.다양한 예외가 발생 가능할 텐데 NOT_FOUND로만 응답하는 것은 아쉬운 일이겠죠.
-// 4. task를 이렇게 찾으면 엄청나게 많은 task가 등록되었을 때 퍼포먼스 이슈가 있을 거에요. 이걸 어떻게 고치면 좋을까요?
-// 6. 응답 코드는 요구사항을 전달해준 사람과 적절히 논의해서 결정하면 됩니다. 여기서 요구사항은 e2e 테스트 코드죠. DELETE에 대한 응답 코드로 200을 쓰는 것이 e2e 테스트 코드를 만족한다면 자유롭게 작업하셔도 문제 없습니다. 자 여기에서 한 가지 더 생각해 볼 문제가 생깁니다.
-//
-//e2e 테스트를 그대로 두고 작업을 할 것인가? 아니면 e2e 테스트를 수정할 것인가?
-
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
@@ -13,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/tasks")
@@ -42,8 +34,11 @@ public class TaskController {
 
   @GetMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
-  public Task targetTask(@PathVariable("id") Long id) {
-
+  public Task targetTask(@PathVariable("id") Long id) throws HttpClientErrorException {
+    if(!tasks.containsKey(id)){
+      throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+    }
+    System.out.println(tasks.get(id));
     return tasks.get(id);
   }
 
@@ -66,11 +61,13 @@ public class TaskController {
     return targetTask;
   }
 
-  @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void delete(@PathVariable("id") Long id) {
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable("id") Long id) throws HttpClientErrorException{
+    if(!tasks.containsKey(id)){
+      throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+    }
     tasks.remove(id);
-    return;
   }
 
 
