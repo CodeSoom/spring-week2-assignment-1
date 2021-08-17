3a873ba8e211ace978e6244d4c7bb0da5bcd7c30
catsbi <killfine198@gmail.com>
2021-08-17 14:32:00 +0900

    refactor: Controller CORS설정 애노테이션 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -14,6 +14,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
     private final TodoRepository todoRepository;
 
