167fc351199af1397d3a0b836cdabf52c2413301
HyoungUkJJang <guddnr1451@gmail.com>
2021-08-17 18:13:03 +0900

    ADD Marko js 작성 및 import 정리
    1. Marko js 입력 및 수정 완료 버튼 생성
    2. 자바 import 정리

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -4,18 +4,26 @@ import com.codesoom.assignment.domain.Task;
 import com.codesoom.assignment.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 
 import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpResponse;
 import java.util.Collection;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
 
     @Autowired
--- app/src/main/java/com/codesoom/assignment/service/TaskServiceImpl.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
 @Component
