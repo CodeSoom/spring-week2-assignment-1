d57d9cacdf6591b345464d96c87e21dda3c5c923
Sungeheon <elon4856@gmail.com>
2021-08-17 17:55:14 +0900

    fix: tasks를 hashmap으로 바꾸세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,7 +2,6 @@
 // 2. @ExceptionHandler가 Exception.class를 갖는 건 처리 범위가 필요 이상으로 커 보이네요. 좀 더 구체화된 예외를 정의하고, 새로 정의한 예외를 사용해 보세요.
 // 3.다양한 예외가 발생 가능할 텐데 NOT_FOUND로만 응답하는 것은 아쉬운 일이겠죠.
 // 4. task를 이렇게 찾으면 엄청나게 많은 task가 등록되었을 때 퍼포먼스 이슈가 있을 거에요. 이걸 어떻게 고치면 좋을까요?
-// 5. 이런 사소한 작업들도 생각보다 중요합니다. 포매터에 대해 조사해 보시고 자동으로 포매터가 작동하게 하거나, 커밋 전에 포매터를 작동시키는 습관을 들여 보세요.
 // 6. 응답 코드는 요구사항을 전달해준 사람과 적절히 논의해서 결정하면 됩니다. 여기서 요구사항은 e2e 테스트 코드죠. DELETE에 대한 응답 코드로 200을 쓰는 것이 e2e 테스트 코드를 만족한다면 자유롭게 작업하셔도 문제 없습니다. 자 여기에서 한 가지 더 생각해 볼 문제가 생깁니다.
 //
 //e2e 테스트를 그대로 두고 작업을 할 것인가? 아니면 e2e 테스트를 수정할 것인가?
@@ -10,8 +9,9 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,28 +29,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
 
   Long id = 0L;
-  private List<Task> tasks = new ArrayList<>();
+  private HashMap<Long, Task> tasks = new HashMap();
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Task> taskList() {
-    return tasks;
+    return tasks
+        .values()
+        .stream()
+        .collect(Collectors.toList());
   }
 
   @GetMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Task targetTask(@PathVariable("id") Long id) {
-    if (findTask(id) == null) {
-      ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
-    }
-    return findTask(id);
+
+    return tasks.get(id);
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Task create(@RequestBody Task task) throws Throwable {
-    task.setId(generateId());
-    tasks.add(task);
+    Long generatedId = generateId();
+    task.setId(generatedId);
+    tasks.put(generatedId,task);
     return task;
   }
 
@@ -59,7 +61,7 @@ public class TaskController {
   @ResponseStatus(HttpStatus.OK)
   public Task rewrite(@RequestBody Task rewrittenTask, @PathVariable("id") Long id)
       throws Throwable {
-    Task targetTask = findTask(id);
+    Task targetTask = tasks.get(id);
     targetTask.setTitle(rewrittenTask.getTitle());
     return targetTask;
   }
@@ -67,7 +69,7 @@ public class TaskController {
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable("id") Long id) {
-    tasks.remove(findTask(id));
+    tasks.remove(id);
     return;
   }
 
@@ -76,9 +78,4 @@ public class TaskController {
     return ++id;
   }
 
-  private Task findTask(Long id) {
-    return tasks.stream()
-        .filter(task -> task.getId().equals(id))
-        .findFirst().orElse(null);
-  }
 }
