db1f9b05933f20088121eb2d8ecf6a6a2955cff6
Sungeheon <elon4856@gmail.com>
2021-08-17 19:29:36 +0900

    fix: exception의 범위를 좁히세요

--- app/src/main/java/com/codesoom/assignment/controllers/ExceptionHandlingController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ExceptionHandlingController.java
@@ -3,13 +3,17 @@ package com.codesoom.assignment.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
 
 @RestControllerAdvice
 class GlobalControllerAdvice {
 
-  @ExceptionHandler(value = Exception.class)
-  public ResponseEntity exception(Exception e) {
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(value = RuntimeException.class)
+  public ResponseEntity notFoundException(Exception e) {
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
   }
