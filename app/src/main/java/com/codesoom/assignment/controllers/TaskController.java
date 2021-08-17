b789176b3c18a873c82de04c027072c764a251f5
injun <ison@sfu.ca>
2021-08-17 20:01:22 +0900

    feat: PATCH, DELETE 구현

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,39 +3,53 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.models.Task;
 import com.codesoom.assignment.repository.TaskRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/tasks")
 @CrossOrigin
 public class TaskController {
 
     TaskRepository taskRepository = new TaskRepository();
 
-    @GetMapping("/tasks")
+    @GetMapping
     public Collection<Task> getTaskList() {
         return taskRepository.getTaskList();
     }
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     public Optional<Task> getTaskById(@PathVariable Long id) {
         return Optional.ofNullable(taskRepository.getTaskById(id));
     }
 
-    @PostMapping("/tasks")
+    @PostMapping
     public Task createTask(@RequestBody Task task) {
         return taskRepository.createTask(task);
     }
 
-    @PutMapping("/tasks")
-    public Task putTask(@RequestBody Task task) {
-        return taskRepository.putTask(task);
+    @PutMapping("/{id}")
+    public Task putTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskRepository.putTask(id, task);
+    }
+
+    @PatchMapping("/{id}")
+    public Optional<Task> patchTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskRepository.patchTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Task> deleteTask(@PathVariable Long id) {
+        return taskRepository.deleteTask(id);
     }
 }
--- app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -5,6 +5,7 @@ import com.codesoom.assignment.models.TaskIdGenerator;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TaskRepository {
@@ -25,8 +26,23 @@ public class TaskRepository {
         return newTask;
     }
 
-    public Task putTask(Task task) {
+    public Task putTask(Long id, Task task) {
         taskMap.put(task.getId(), task);
         return task;
+    }
+
+    public Optional<Task> patchTask(Long id, Task task) {
+        Optional<Task> oldTask = Optional.ofNullable(taskMap.get(id));
+        oldTask.ifPresent(item -> item.setTitle(task.getTitle()));
+        return oldTask;
+    }
+
+    public Optional<Task> deleteTask(Long id) {
+        Task task = null;
+        if (taskMap.containsKey(id)) {
+            task = taskMap.get(id);
+            taskMap.remove(id);
+        }
+        return Optional.ofNullable(task);
     }
 }
