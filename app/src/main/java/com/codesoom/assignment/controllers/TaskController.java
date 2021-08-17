3539f800107a68e07cbc452eedad67ac3917c718
daeungkim <daeungkim93@gmail.com>
2021-08-17 20:36:53 +0900

    "/tasks/{id}" DELETE 요청 처리 메서드 추가
    "/tasks/{id}" DELETE 요청 처리 메서드 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.model.Task;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 @CrossOrigin
 @RestController
@@ -22,6 +25,8 @@ import java.util.Optional;
 public class TaskController {
     private static final String ROOT_PATH = "";
     private static final String ID_PATH = "/{id}";
+    private static final String ID = "id";
+    private static final int INDEX_START = 0;
 
     private final List<Task> tasks = new ArrayList<>();
     private Long newId = 0L;
@@ -44,11 +49,19 @@ public class TaskController {
 
     @PatchMapping(ID_PATH)
     @PutMapping(ID_PATH)
-    public Task updateTask(@PathVariable("id") final Long id, @RequestBody final Task newTask) {
+    public Task updateTask(@PathVariable(ID) final Long id, @RequestBody final Task newTask) {
         Optional<Task> taskOptional = tasks.stream()
                 .filter(task -> Objects.equals(task.getId(), id))
                 .findFirst();
         taskOptional.ifPresent(task -> task.setTitle(newTask.getTitle()));
         return taskOptional.get();
+    }
+
+    @DeleteMapping(ID_PATH)
+    public void deleteTask(@PathVariable(ID) final Long id) {
+        OptionalInt indexOptional = IntStream.range(INDEX_START, tasks.size())
+                .filter(index -> Objects.equals(tasks.get(index).getId(), id))
+                .findFirst();
+        indexOptional.ifPresent(tasks::remove);
     }
 }
