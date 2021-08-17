43abbaaa76478564f846584f417635cc6526e52d
daeungkim <daeungkim93@gmail.com>
2021-08-17 09:14:23 +0900

    코드숨 2주차 스프링 웹 강의 수강 및 반영
    코드숨 2주차 스프링 웹 강의 수강 및 반영

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.model.Task;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
+    @GetMapping("")
+    public List<Task> sayHello() {
+        return tasks;
+    }
+
+    @PostMapping("")
+    public Task postTask(@RequestBody final Task task) {
+        task.setId(++newId);
+        return task;
+    }
+}
