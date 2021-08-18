bba5324a688bd13b7d6e38804df93f8c4e051bc1
wjy5446 <wjy5446@gmail.com>
2021-08-18 22:19:13 +0900

    feat) ResponseEntity를 이용한 HttpStatus 설정

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,9 +2,12 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.TaskManager;
 import com.codesoom.assignment.models.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,36 +16,57 @@ public class TaskController {
     TaskManager taskManager = new TaskManager();
 
     @GetMapping("/**")
-    public List<Task> getAll() {
-        return taskManager.getAll();
+    public ResponseEntity<List<Task>> getAll() {
+        return new ResponseEntity<>(taskManager.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{taskId}")
-    public Task getOne(@PathVariable String taskId) {
-        return taskManager.getOne(Long.parseLong(taskId));
+    public ResponseEntity<?> getOne(@PathVariable Long taskId) {
+        Task task;
+
+        try {
+            task = taskManager.getOne(taskId);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
     @PostMapping("/**")
-    public Task create(@RequestBody Task task) {
+    public ResponseEntity<Task> create(@RequestBody Task task) {
         String title = task.getTitle();
+
         taskManager.create(title);
-        return taskManager.getLast();
+
+        return new ResponseEntity<>(taskManager.getLast(), HttpStatus.CREATED);
     }
 
     @PutMapping("/{taskId}")
     @PatchMapping("/{taskId}")
-    public Task update(@PathVariable String taskId, @RequestBody Task task) {
-        String title = task.getTitle();
-        Long id = Long.parseLong(taskId);
-        taskManager.update(id, title);
-        return taskManager.getOne(id);
+    public ResponseEntity<?> update(@PathVariable Long taskId, @RequestBody Task task) {
+        Task updatedTask;
+
+        try {
+            String title = task.getTitle();
+            taskManager.update(taskId, title);
+            updatedTask = taskManager.getOne(taskId);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(updatedTask, HttpStatus.OK);
     }
 
     @DeleteMapping("/{taskId}")
-    public String delete(@PathVariable String taskId) {
-        Long id = Long.parseLong(taskId);
-        taskManager.remove(id);
-        return "Success Remove Task";
+    public ResponseEntity<?> delete(@PathVariable Long taskId) {
+        try {
+            taskManager.remove(taskId);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>("Success Remove Task", HttpStatus.NO_CONTENT);
     }
 }
 
