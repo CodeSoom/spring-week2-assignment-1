9ae932035e1a658f60b9f2a2cda20f57392791bc
wjy5446 <wjy5446@gmail.com>
2021-08-21 03:05:17 +0900

    refac) javadoc과 실제 코드 사이의 공백 제거

--- app/src/main/java/com/codesoom/assignment/Exception/TaskNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/Exception/TaskNotFoundException.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.Exception;
 /**
  * 할 일을 찾지 못했을 때 던집니다.
  */
-
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException() {
         super();
