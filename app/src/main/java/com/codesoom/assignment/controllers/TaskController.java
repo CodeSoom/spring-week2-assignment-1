bd7099af260a49a61ad8984973646a1a6c73e520
daeungkim <daeungkim93@gmail.com>
2021-08-20 22:07:31 +0900

    Task확인 작업 수행 클래스 변경
    "tasks/{id}" 요청
    id를 이용하여 Task가 존재하는지 확인하는 작업
    수행하는 클래스 TaskController -> IdInterceptor로 변경
    -> 코드 중복이 발생하므로

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,7 +1,6 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dtos.TaskDTO;
-import com.codesoom.assignment.exceptions.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
 import com.codesoom.assignment.repositories.TaskRepository;
 import org.springframework.http.HttpStatus;
@@ -16,12 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.stream.IntStream;
 
 @CrossOrigin
 @RestController
@@ -44,18 +39,12 @@ public final class TaskController {
     @GetMapping(ID_PATH)
     public Task getTask(@PathVariable(ID) final Long id) {
         final OptionalInt optionalInt = TaskRepository.findTaskIndex(id);
-        if (optionalInt.isEmpty()) {
-            throw new TaskNotFoundException();
-        }
         return TaskRepository.getTask(optionalInt.getAsInt());
     }
 
     @RequestMapping(value = ID_PATH, method = {RequestMethod.PUT, RequestMethod.PATCH})
     public Task updateTask(@PathVariable(ID) final Long id, @RequestBody final TaskDTO taskDTO) {
         final OptionalInt optionalInt = TaskRepository.findTaskIndex(id);
-        if (optionalInt.isEmpty()) {
-            throw new TaskNotFoundException();
-        }
         return TaskRepository.updateTask(optionalInt.getAsInt(), taskDTO);
     }
 
@@ -63,9 +52,6 @@ public final class TaskController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTask(@PathVariable(ID) final Long id) {
         final OptionalInt optionalInt = TaskRepository.findTaskIndex(id);
-        if (optionalInt.isEmpty()) {
-            throw new TaskNotFoundException();
-        }
         TaskRepository.deleteTask(optionalInt.getAsInt());
     }
 }
--- app/src/main/java/com/codesoom/assignment/interceptors/IdInterceptor.java
+++ app/src/main/java/com/codesoom/assignment/interceptors/IdInterceptor.java
@@ -1,13 +1,26 @@
 package com.codesoom.assignment.interceptors;
 
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.Map;
+import java.util.OptionalInt;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public final class IdInterceptor extends HandlerInterceptorAdapter {
+import com.codesoom.assignment.exceptions.TaskNotFoundException;
+import com.codesoom.assignment.repositories.TaskRepository;
+
+public final class IdInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
-        return super.preHandle(request, response, handler);
+        final Map<String, String> pathVariable = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        final Long id = Long.parseLong(pathVariable.get("id"));
+        final OptionalInt optionalInt = TaskRepository.findTaskIndex(id);
+        if (optionalInt.isEmpty()) {
+            throw new TaskNotFoundException();
+        }
+        return true;
     }
 }
