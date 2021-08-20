180c4b6788d3369d37cf805be64c3d226242d7ed
Juyeong Park <sprnd645@gmail.com>
2021-08-20 22:14:10 +0900

    update: task Id를 찾지 못할 시 에러 메시지 보충
    Co-authored-by: JohnGrib <johngrib.lee@kurlycorp.com>

--- app/src/main/java/com/codesoom/assignment/errors/TaskIdNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/TaskIdNotFoundException.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class TaskIdNotFoundException extends RuntimeException {
 
     public TaskIdNotFoundException(Long id) {
-        super(String.format("id %s 를 찾을 수 없습니다.", id));
+        super(String.format("id %s 에 해당하는 할 일을 찾을 수 없습니다.", id));
     }
 }
