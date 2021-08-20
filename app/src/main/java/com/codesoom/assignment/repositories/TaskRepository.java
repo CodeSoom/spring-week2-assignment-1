758f45f0df88cc802bdbfea53ee5406451cb4eee
pangnem <sprnd645@gmail.com>
2021-08-20 23:00:45 +0900

    docs: TaskRepository javadoc 추가

--- app/src/main/java/com/codesoom/assignment/repositories/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repositories/TaskRepository.java
@@ -16,18 +16,42 @@ public class TaskRepository {
 
     private final Map<Long, Task> taskMap = new HashMap<>();
 
+    /**
+     * 번호와 할 일을 받아 저장합니다.
+     *
+     * @param id 저장할 번호
+     * @param task 저장할 할 일
+     */
     public void save(Long id, Task task) {
         taskMap.put(id, task);
     }
 
+    /**
+     * 모든 할 일을 찾습니다.
+     *
+     * @return 할 일 목록
+     */
     public Collection<Task> getAllTasks() {
         return taskMap.values();
     }
 
+    /**
+     * 번호에 해당하는 할 일을 찾습니다.
+     *
+     * @param id 찾을 할 일 번호
+     * @return Task
+     */
     public Task get(Long id) {
         return findWith(id);
     }
 
+    /**
+     * 번호로 해당 할 일을 찾고, 제목을 변경합니다.
+     *
+     * @param id 찾을 할 일 번호
+     * @param title 변경할 할 일 제목
+     * @return Task
+     */
     public Task update(Long id, String title) {
         Task task = findWith(id);
 
@@ -36,6 +60,11 @@ public class TaskRepository {
         return task;
     }
 
+    /**
+     * 번호로 할 일을 찾고, 삭제합니다.
+     *
+     * @param id 삭제할 번호
+     */
     public void delete(Long id) {
         findWith(id);
 
