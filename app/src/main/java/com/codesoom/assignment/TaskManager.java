b4c8ae7abab0636c1bbefcdcef6089391be7fde4
pangnem <sprnd645@gmail.com>
2021-08-20 13:39:02 +0900

    remove: 불필요한 설명 제거

--- app/src/main/java/com/codesoom/assignment/TaskManager.java
+++ app/src/main/java/com/codesoom/assignment/TaskManager.java
@@ -5,15 +5,6 @@ import java.util.Collection;
 
 /**
  * 할 일 목록 관리를 담당합니다.
- *
- * <p>
- * {@link com.codesoom.assignment.controllers.TasksController TasksController}에서 요청을 받고,
- * {@link TaskMap TaskMap}에 실제 할 작업을 요청하고 결과를 가져옵니다.
- * </p>
- *
- * @author pangnem
- * @see com.codesoom.assignment.controllers.TasksController TasksController
- * @see TaskMap
  */
 public class TaskManager {
 
