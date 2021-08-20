2e7c65340b4083f3ca280161126ab114504ae5c8
catsbi <killfine1988@gmail.com>
2021-08-20 15:27:42 +0900

    refactor: 예외 메세지 좀 더 명시적으로 변경

--- app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/TaskNotFoundException.java
@@ -1,8 +1,8 @@
 package com.codesoom.assignment.exceptions;
 
 public class TaskNotFoundException extends RuntimeException{
-    public static final String DEFAULT_MESSAGE_FORMAT = "아이디가 [%d]인 요소를 찾을 수 없습니다.";
-    public static final String DEFAULT_MESSAGE = "요소를 찾을 수 없습니다.";
+    public static final String DEFAULT_MESSAGE_FORMAT = "아이디가 [%d]인 할 일을 찾을 수 없습니다.";
+    public static final String DEFAULT_MESSAGE = "할 일을 찾을 수 없습니다.";
 
     public TaskNotFoundException() {
         super(DEFAULT_MESSAGE);
