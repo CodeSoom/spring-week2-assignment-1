c7b1e1f0ae3bbc6a8fded84c19e3d14969fca422
Sungeheon <elon4856@gmail.com>
2021-08-21 20:50:21 +0900

    fix: GlobalControllerAdvice를 좀더 디테일하게 TaskControllerAdvice로 수정하세요

--- app/src/main/java/com/codesoom/assignment/controllers/ExceptionHandlingController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ExceptionHandlingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-class GlobalControllerAdvice {
+class TaskControllerAdvice {
 
   @RequestMapping({"/{id}"})
   @ResponseStatus(HttpStatus.NOT_FOUND)
