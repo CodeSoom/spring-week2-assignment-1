c0f79844b45cb790eb385eea2c6ca30a5d7bd608
bloomspes <bloomspes@gmail.com>
2021-08-21 20:55:16 +0900

    @RequestBody 어노테이션으로 title 객체 가져오기. - public create의 파라미터로 Task 객체 선언 - 그대로 실행시, setTitle의 title 값이 null 값으로 전달 되는 것을 확인 - title의 객체를 가져오기 위해 파라미터 Task 앞에 @RequestBody 선언.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -10,6 +10,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.models.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +29,7 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task create() {
-        Task task = new Task();
+    public Task create(@RequestBody Task task) {
         task.setId(generateId());
         tasks.add(task);
         
