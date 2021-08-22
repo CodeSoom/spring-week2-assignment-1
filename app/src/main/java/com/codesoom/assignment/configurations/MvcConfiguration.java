ace74dbcdda634161b4ea5b77b06794219c3ba3c
daeungkim <daeungkim93@gmail.com>
2021-08-22 19:18:36 +0900

    주석 추가
    주석 추가
    -> 해당 메서드가 어떤 역할을 하는지 직관적으로 이해가 안 될 수 있다 판단

--- app/src/main/java/com/codesoom/assignment/configurations/MvcConfiguration.java
+++ app/src/main/java/com/codesoom/assignment/configurations/MvcConfiguration.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+    /**
+     * "/tasks/{id}"로의 http 요청 시
+     * IdInterceptor의 preHandle 메서드가 TaskController보다 먼저 불리도록 설정
+     */
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new IdInterceptor()).addPathPatterns("/tasks/{id}");
--- app/src/main/java/com/codesoom/assignment/interceptors/IdInterceptor.java
+++ app/src/main/java/com/codesoom/assignment/interceptors/IdInterceptor.java
@@ -16,6 +16,10 @@ import com.codesoom.assignment.utils.TaskConstants;
 public final class IdInterceptor implements HandlerInterceptor {
     private final TaskRepository taskRepository = TaskRepository.getInstance();
 
+    /**
+     * "/tasks/{id}" http 요청 시
+     * id에 해당하는 Task가 존재하는지 확인하는 메서드
+     */
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
         final Map<String, String> pathVariable = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
