bb2e25ba04960fb5d06ecb079c1c0c9480ed866c
bloomspes <bloomspes@gmail.com>
2021-08-25 11:57:12 +0900

    TaskRepository 객체에 final 키워드를 명시함.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
     public TaskController(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
