43f0bb6f2d819c71447df80dafa0e6e7082b3ef2
hyuk <pjh08190819@naver.com>
2021-12-08 01:46:02 +0900

    ResponseState 어노테이션 사용해보기

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,20 +3,21 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.domain.Task;
 import com.codesoom.assignment.service.TaskService;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -33,9 +34,10 @@ public class TaskController {
     }
 
     @GetMapping
-    public ResponseEntity list() {
+    @ResponseStatus(HttpStatus.OK)
+    public List<Task> list() {
 
-        return ResponseEntity.ok(taskService.findALL());
+        return taskService.findALL();
     }
 
     @GetMapping("/{id}")
