6564efaaab65ae7e9dbf4adb681c479f4b6c6c00
Kwakcena <rhkrgudwh@gmail.com>
2021-12-08 13:59:09 +0900

    할 일을 추가하는 POST api를 만들어라
    할 일을 추가하는 POST api를 만들었습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,14 +3,12 @@ package com.codesoom.assignment.controllers;
 // TODO
 // 1. GET /tasks (done)
 // 2. GET /tasks/{id}
-// 3. POST /tasks
+// 3. POST /tasks (done)
 // 4. PUT/PATCH /tasks/{id}
 // 5. DELETE /tasks/{id}
 
 import com.codesoom.assignment.models.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +17,24 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskController {
     private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
 
     @GetMapping
     public List<Task> list() {
         return tasks;
     }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        task.setId(generateId());
+        tasks.add(task);
+        return task;
+    }
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
+    }
+
 
 }
