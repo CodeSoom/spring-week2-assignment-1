a0c8c54270194e95c9f040489d7199017f35ff1e
Kwakcena <rhkrgudwh@gmail.com>
2021-12-09 00:42:01 +0900

    import * 의 사용을 지양하라
    import * 를 하지 않고, 내가 사용할 것들을 명시적으로 보여준다.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -9,12 +9,20 @@ package com.codesoom.assignment.controllers;
 //  -> PUT은 어떻게 하지? PUT과 PATCH mapping을 같이 쓸 순 없나?
 // 5. DELETE /tasks/{id} (done)
 // 6. taskService를 만들기 (done)
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.codesoom.assignment.models.Task;
-import org.springframework.web.bind.annotation.*;
+import com.codesoom.assignment.services.TaskService;
 
 import java.util.List;
-import com.codesoom.assignment.services.TaskService;
 
 @RestController
 @RequestMapping("/tasks")
