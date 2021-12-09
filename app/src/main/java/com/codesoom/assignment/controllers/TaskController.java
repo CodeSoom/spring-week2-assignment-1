4a22679d972a42d4f5ab00ee2c6baf665bf02998
hyeongjoGwak <hyeongjo.Gwak@kurlycorp.com>
2021-12-10 07:49:12 +0900

    update 메소드에 PUT, PATCH를 같이 할당한다
    RequestMapping을 이용하면 여러개의 메소드를 매칭시킬 수 있다.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -6,17 +6,17 @@ package com.codesoom.assignment.controllers;
 //  -> 없을 경우 not found 처리
 // 3. POST /tasks (done)
 // 4. PUT/PATCH /tasks/{id} (done)
-//  -> PUT은 어떻게 하지? PUT과 PATCH mapping을 같이 쓸 순 없나?
+//  -> PUT은 어떻게 하지? PUT과 PATCH mapping을 같이 쓸 순 없나? (done)
 // 5. DELETE /tasks/{id} (done)
 // 6. taskService를 만들기 (done)
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codesoom.assignment.models.Task;
@@ -45,7 +45,7 @@ public class TaskController {
         return taskService.createTask(task);
     }
 
-    @PatchMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
     public Task update(@PathVariable Long id, @RequestBody Task source) {
         return taskService.updateTask(id, source);
     }
