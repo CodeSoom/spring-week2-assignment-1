cce4e583dc94d10fea1a4728c9cd1f79ded57fa6
hyuk <pjh08190819@naver.com>
2021-12-10 14:13:02 +0900

    ListStorageRepository 부모 클래스 네임 변경

--- app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -2,11 +2,11 @@ package com.codesoom.assignment.repository;
 
 
 import com.codesoom.assignment.domain.Task;
-import com.codesoom.assignment.storage.ListStorageRepositoryAbstract;
+import com.codesoom.assignment.storage.ListStorageRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TaskRepository extends ListStorageRepositoryAbstract<Task> {
+public class TaskRepository extends ListStorageRepository<Task> {
 
     public Task update(Task target, Task source) {
         target.setTitle(source.getTitle());
