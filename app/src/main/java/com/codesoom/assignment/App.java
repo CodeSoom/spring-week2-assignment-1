c98cf900ce5ffec815751d7228adcefc24f3dc65
HYUNA <hyunaseodev@gmail.com>
2021-12-11 02:29:49 +0900

    수정 버튼 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "Hello, world!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args); //어떻게 run을 할 것인지, App.class ->  어떤 클래스로 실행할 것임
     }
 }
