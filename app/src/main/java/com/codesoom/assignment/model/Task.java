3f2658b571016bb425a99ad6a3c0199a225fbdbf
kyuwon53 <lkw2v@naver.com>
2021-12-11 21:45:03 +0900

    [Add] 상태 정보를 추가하라

--- app/src/main/java/com/codesoom/assignment/model/Task.java
+++ app/src/main/java/com/codesoom/assignment/model/Task.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.model;
 public class Task {
     private String title;
     private Long id;
+    private String state;
 
     public String getTitle() {
         return title;
@@ -20,11 +21,20 @@ public class Task {
         this.id = id;
     }
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
     @Override
     public String toString() {
         return "Task{" +
-                "id=" + id +
-                " , title='" + title + '\'' +
+                "id='" + id + '\'' +
+                ", title=" + title +
+                ", state=" + state +
                 '}';
     }
 }
