2caf911f8cf303b5fd672fd186099ff9addbcee0
HYUNA <hyunaseodev@gmail.com>
2021-12-11 23:10:08 +0900

    Spring을 활용하여 GET /tasks/{id}, DELETE /tasks 구현
    추가로 PATCH /tasks에서 예외발생시 해결하는 코드 작성

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,11 +1,9 @@
 //TODO
 // 1. Read Collection - GET /tasks => 완료
-// 2. Read Item - GET /tasks/
+// 2. Read Item - GET /tasks/{id} => 완료
 // 3. Create - POST /tasks => 완료
-// 4. Update - PUT/PATCH /tasks
-//버튼 추가 - (조회)수정 / (수정시)확인, 취소
-// 5. DELETE - DELETE /tasks
-//버튼 추가 - (조회)완료
+// 4. Update - PUT/PATCH /tasks => 완료
+// 5. DELETE - DELETE /tasks => 완료
 
 package com.codesoom.assignment.controllers;
 
@@ -29,6 +27,57 @@ public class TaskController {
     @GetMapping //훨씬 더 수월함.
     public List<Task> list() {
         return tasks;
+    }
+
+    /*
+    1. 이해
+     * 우리가 풀어야 될 문제가 무엇인지?
+        - 할 일 목록에서 특정 id와 일치하는 할 일을 조회한다.
+     * 우리가 알고있는 사실들은 어떤 것들이 있는지
+        - 저장되어 있는 할 일 목록이 있다. 할 일 목록은 List<Task>다.
+        - Task가 이미 정의되어 있다.
+        - 요청으로는 Task를 요청한다.
+        - 요청시 Long 타입 파라미터를 보낸다.
+        - 응답으로는 id가 요청시 받은 파라미터와 일치하는 Task를 응답한다.
+        - HTTP Method는 GET으로 요청이 온다.
+     * 우리가 아직 모르는 것들
+        - 사용자가 입력하는 데이터는 어떤 모양인가?
+     * 비슷한 문제를 풀어본 적이 있는지?
+        - 회사에서 데이터를 수정해 본 경험있음.
+     * 우리가 문제를 풀 때 어떤 조건들이 있는지?
+        - 파라미터는 정수 타입만 가능하다.
+        - 요청시 넘긴 파라미터와 일치하는 할 일이 없을 경우 반환할 Task가 없다.
+    2. 계획
+        - 사용자의 요청에 대해서 제약(파라미터는 정수 타입만 가능)을 건다.
+        - 파라미터를 서버로 넘기는 작업
+        - 받은 파라미터와 일치하는 할 일 찾기
+        - 찾은 할 일을 응답한다.
+
+        - id를 받는다.
+        - 우리가 가진 할 일 목록에서, id 값으로 Task를 찾는다.
+            - 할 일 목록에서 어떻게 찾는가? => for문으로 찾는다.
+        - 찾은 Task를 응답한다.
+    3. 실행
+    4. 회고
+        - 어떻게 하면 더 효과적으로 해결할 수 있었을까?
+            for문을 사용하여 할 일 목록을 찾는 경우 오래걸릴 가능성 있음. 속도개선을 위해 다른 방법 고민 및 활용하기.
+        - 어떻게 하면 더 효율적으로 해결할 수 있었을까?
+            파라미터를 받는 방법은 @PathVariable 와 @RequestParam
+            둘의 차이를 자세히 알아본 후 어느 방법이 더 효율적인지 정한 후 적용하기.
+    */
+    @GetMapping("/{paramId}")
+    public Task getTaskById(@PathVariable(name = "paramId") long id) {
+        // id를 받는다.
+        // 우리가 가진 할 일 목록에서, id 값으로 Task를 찾는다.
+        // 할 일 목록에서 어떻게 찾는가? => for문으로 찾는다.
+        Task task = null;
+        for(int i = 0; i < tasks.size(); i++) {
+            if(tasks.get(i).getId() == id){
+                task = tasks.get(i);
+            }
+        }
+        // 찾은 Task를 응답한다.
+        return task;
     }
 
     //@RequestMapping(path = "", method = RequestMethod.POST)
@@ -124,11 +173,68 @@ public class TaskController {
         //   * 할 일 목록에서 어떻게 찾는가? => indexOf()로 찾는
         int taskIndex = tasks.indexOf(task);
         // TODO: 찾을 수 없었을 때 어떻게 할 것인가?
-        Task foundTask = tasks.get(taskIndex);
-        // * 찾은 Task를 수정해요.
-        foundTask.setTitle(task.getTitle());
+        // 찾을 수 없는 경우 -1 반환. IndexOutOfBoundsException 오류 발생.
+        // IndexOutOfBoundsException 오류 해결하기 위해 if문 사용.
+        // if문 조건식인 index 값이 -1이 아닌 경우 문장 수행
+        Task foundTask = null;
+        if(taskIndex != -1){
+            tasks.get(taskIndex);
+            // * 찾은 Task를 수정해요.
+            foundTask.setTitle(task.getTitle());
+        }
         // * 수정된 Task를 응답한다.
         return foundTask;
+    }
+
+    /*
+        1. 이해
+         * 우리가 풀어야 될 문제가 무엇인지?
+            - 할 일을 삭제하고 삭제 후 재조회시 할 일 목록에서 삭제한 항목이 보이지 않아야 한다.
+
+         * 우리가 알고있는 사실들은 어떤 것들이 있는지
+            - 저장되어 있는 할 일 목록이 있다. 할 일 목록은 List<Task>이다.
+            - Task가 이미 정의되어 있다.
+            - 요청으로는 Task를 요청한다.
+            - 응답으로는 Task를 반환할 필요가 없다.
+            - HTTP Method는 DELETE로 요청이 온다.
+            - Spring Annotation을 활용할 수 있다.
+
+         * 우리가 아직 모르는 것들
+            - 어떤 Task를 삭제할지 어떻게 알 수 있을까?
+                - Task에 id를 포함해서 전송해야 한다.
+
+         * 비슷한 문제를 풀어본 적이 있는지?
+            - 회사에서 데이터를 삭제하는 일을 많이 해봤다.
+         * 우리가 문제를 풀 때 어떤 조건들이 있는지?
+            - 삭제시 삭제 요청한 Task만 삭제되고 나머지 할 일은 삭제되면 안 된다.
+
+        2. 계획
+            - 삭제 요청한 Task를 서버로 넘기는 작업
+
+            - Task를 받는다.
+            - 우리가 가진 할 일 목록에서, Task를 찾는다.
+                할 일 목록에서 어떻게 찾는가? => indexOf()찾는다.
+            - 찾은 Task를 삭제한다.
+                찾은 Task를 어떻게 삭제하는가? => remove() 로 삭제한다.
+        3. 실행
+        4. 회고
+            - 어떻게 하면 더 효과적으로 해결할 수 있었을까?
+            - 어떻게 하면 더 효율적으로 해결할 수 있었을까?
+    */
+    @DeleteMapping
+    public void delete(@RequestBody Task task){
+        /*
+        - 삭제 요청한 Task를 서버로 넘기는 작업
+
+        - Task를 받는다.
+        - 우리가 가진 할 일 목록에서, Task를 찾는다.
+        할 일 목록에서 어떻게 찾는가? => indexOf()찾는다.
+        */
+        int taskIndex = tasks.indexOf(task);
+        Task foundTask = tasks.get(taskIndex);
+        // 찾은 Task를 삭제한다.
+        //  찾은 Task를 어떻게 삭제하는가? => remove() 로 삭제한다.
+        tasks.remove(foundTask);
     }
 
     private Long generate(){
