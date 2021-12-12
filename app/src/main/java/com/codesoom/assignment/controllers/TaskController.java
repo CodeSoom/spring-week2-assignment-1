377e6e85b2baf4cb2b0d0a019b6ba0465ccfe6f5
kyuwon53 <lkw2v@naver.com>
2021-12-12 12:51:46 +0900

    [Fix] Service와 Controller를 분리하라

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.model.Task;
 
+import com.codesoom.assignment.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,61 +27,40 @@ import java.util.Optional;
 @CrossOrigin
 public class TaskController {
 
-    private Map<Long, Task> tasks = new LinkedHashMap<Long, Task>();
-    private Long newId = 0L;
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
 
     @GetMapping
     public List<Task> list() {
 
-        return new ArrayList<Task>(tasks.values());
+        return taskService.list();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Task create(@RequestBody Task task) {
-        Long id = generateId();
-        task.setId(id);
-        task.setState("yet");
-        tasks.put(id, task);
 
-        return task;
+        return taskService.create(task);
     }
 
     @GetMapping("/{id}")
     public Optional<Task> view(@PathVariable Long id) {
-        Optional<Task> task = findTask(id);
-        if (!task.isPresent()) {
-            throw new TaskNotFoundException("Task가 없습니다.");
-        }
-        return task;
+
+        return taskService.view(id);
     }
 
     @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH}, value = "/{id}")
-    public Optional<Task> update(@RequestBody Task body, @PathVariable Long id) {
-        Optional<Task> task = findTask(id);
-        task.get().setTitle(body.getTitle());
-        task.get().setState(body.getState());
-        return task;
+    public Optional<Task> update(@RequestBody Task task, @PathVariable Long id) {
+
+        return taskService.update(task, id);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        Optional<Task> task = findTask(id);
-        if (!task.isPresent()) {
-            throw new TaskNotFoundException("요청하신 " + id + "의 Task가 없습니다.");
-        }
-        tasks.remove(id);
-    }
 
-    private synchronized Long generateId() {
-        newId += 1;
-        return newId;
-    }
-
-    private Optional<Task> findTask(Long id) {
-        if (id == null) {
-            throw new IllegalArgumentException("id가 null이므로 Task를 찾을 수 없습니다.");
-        }
-        return Optional.ofNullable(tasks.get(id));
+        taskService.delete(id);
     }
 }
