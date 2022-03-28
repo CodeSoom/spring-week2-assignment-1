c797d37fe2fee7c636a65f28d8aa9f3b6b70deae
injae7034 <injae7034@gmail.com>
2022-03-28 15:53:02 +0900

    강의대로 따라하기

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,45 @@
+//TODO
+//1. Read Collection - GET /tasks
+//2. Read Item - GET /tasks/{id}
+//3. Create - POST /tasks
+//4. Update - PUT/PATCH /tasks/{id}
+//5. Delete - DELETE/tasks/{id}
+
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin
+public class TaskController {
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
+    @GetMapping
+    public List<Task> list() {
+        return tasks;
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        task.setId(generateId());
+        tasks.add(task);
+
+        return task;
+    }
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
+    }
+}
