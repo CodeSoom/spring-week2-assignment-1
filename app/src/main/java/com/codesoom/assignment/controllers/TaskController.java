db708de45cded471416300d92a1510ed14427309
shineoov <shineoov88@gmail.com>
2022-03-28 20:57:38 +0900

    feat: Cors 허용 설정

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/tasks")
 @RestController
 public class TaskController {
