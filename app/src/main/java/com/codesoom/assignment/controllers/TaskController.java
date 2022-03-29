fa8e429eeab4e9e9e4cb5380d7bc8bac28cd5c53
minhee.son <minhee4735@gmail.com>
2022-03-30 06:26:29 +0900

    refactor PUT, PATCH 두 매소드 하나의 함수에 매핑

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -6,11 +6,11 @@ import com.codesoom.assignment.services.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,21 +43,8 @@ public class TaskController {
         return taskService.addTask(newTask);
     }
 
-    @PutMapping("/tasks/{taskId}")
+    @RequestMapping(value = "/tasks/{taskId}", method = {RequestMethod.PUT, RequestMethod.PATCH})
     public BaseResponse<Task> editTask(@PathVariable Long taskId, @RequestBody Task task) {
-        if (isInValidTaskId(taskId)) {
-            return new BaseResponse<>(HttpStatus.NOT_FOUND);
-        }
-
-        if (!task.hasValidContent()) {
-            return new BaseResponse<>(HttpStatus.BAD_REQUEST);
-        }
-
-        return taskService.editTask(taskId, task);
-    }
-
-    @PatchMapping("/tasks/{taskId}")
-    public BaseResponse<Task> editTaskElement(@PathVariable Long taskId, @RequestBody Task task) {
         if (isInValidTaskId(taskId)) {
             return new BaseResponse<>(HttpStatus.NOT_FOUND);
         }
