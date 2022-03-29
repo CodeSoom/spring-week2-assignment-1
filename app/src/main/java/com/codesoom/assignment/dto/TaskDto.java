5ca6b629a7a585c0fc7588a4f7cc515db68e9faf
5Hanui <gkdud644@naver.com>
2022-03-29 18:28:58 +0900

    TaskDto 추가

---
+++ app/src/main/java/com/codesoom/assignment/dto/TaskDto.java
@@ -0,0 +1,14 @@
+package com.codesoom.assignment.dto;
+
+public class TaskDto {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
