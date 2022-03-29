491af4d7120317938c2f2378ad62f4a0c0038cf7
5Hanui <gkdud644@naver.com>
2022-03-30 01:35:07 +0900

    taskId 생성메소드 synchronized 적용

--- app/src/main/java/com/codesoom/assignment/repository/TodoRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TodoRepository.java
@@ -3,21 +3,17 @@ package com.codesoom.assignment.repository;
 import com.codesoom.assignment.models.Task;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class TodoRepository {
 
     private List<Task> tasks = new ArrayList<>();
+    private Long lastId = 0L;
 
-    private Long getNewId() {
-        if(tasks.isEmpty()) return 1L;
-        Task task = tasks.stream()
-                .sorted(Comparator.comparing(Task::getId).reversed())
-                .findFirst()
-                .get();
-        return task.getId() + 1L;
+    private synchronized Long getNewId() {
+        lastId += 1L;
+        return lastId;
     }
 
     public List<Task> findAllTasks() {
