8749af8a3f998f4cb803a0c8950eaf0d42dee245
davidyang2149 <davidyang2149@gmail.com>
2022-03-30 23:45:11 +0900

    Todo 수정 시 PATCH도 사용할 수 있도록 개선합니다

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,15 +5,16 @@ import com.codesoom.assignment.models.TaskStorage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @RestController
 @RequestMapping("/tasks")
@@ -36,7 +37,7 @@ public class TaskController {
         return taskStorage.create(task);
     }
 
-    @PutMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = { PUT, PATCH })
     public Task update(@PathVariable Long id, @RequestBody Task task) {
         return taskStorage.update(id, task.getTitle());
     }
