bcc8d099a3f4f3963c16d7b604ae89c60cfd4bf2
jakeseo <n00nietzsche@gmail.com>
2022-03-30 20:43:25 +0900

    AutoConfigureUtf8MockMvc 주석 추가

--- app/src/test/java/com/codesoom/assignment/annotations/AutoConfigureUtf8MockMvc.java
+++ app/src/test/java/com/codesoom/assignment/annotations/AutoConfigureUtf8MockMvc.java
@@ -13,6 +13,12 @@ import java.nio.charset.StandardCharsets;
 @Documented
 @AutoConfigureMockMvc
 @Import(AutoConfigureUtf8MockMvc.Config.class)
+/**
+ * @AutoConfigureMockMvc 에 UTF-8 인코딩 필터를 적용한다.
+ *
+ * Spring 에 존재하던 APPLICATION_JSON_UTF8 상수가 Deprecated 되어
+ * @AutoConfigureMockMvc 를 이용할 경우, 한글 인코딩 문제가 발생한다.
+ */
 public @interface AutoConfigureUtf8MockMvc {
     class Config {
         @Bean
