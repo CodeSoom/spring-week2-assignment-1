75e79f1149432d2aac09ce3c32c2bf9042887407
davidyang2149 <davidyang2149@gmail.com>
2022-03-31 23:30:45 +0900

    반환값에 HTTP 상태 코드를 추가합니다

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
@@ -23,26 +27,31 @@ public class TaskController {
     private final Tasks taskBookKeeper = new Tasks();
 
     @GetMapping
+    @ResponseStatus(OK)
     public List<Task> list() {
         return taskBookKeeper.readAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(OK)
     public Task read(@PathVariable Long id) {
         return taskBookKeeper.read(id);
     }
 
     @PostMapping
+    @ResponseStatus(CREATED)
     public Task create(@RequestBody Task task) {
         return taskBookKeeper.create(task);
     }
 
     @RequestMapping(value = "/{id}", method = { PUT, PATCH })
+    @ResponseStatus(OK)
     public Task update(@PathVariable Long id, @RequestBody Task task) {
         return taskBookKeeper.update(id, task.getTitle());
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(NO_CONTENT)
     public void delete(@PathVariable Long id) {
         taskBookKeeper.delete(id);
     }
