9cc2a15ba038ff06cb34b83f2d26335b8adb267f
injae7034 <injae7034@gmail.com>
2022-03-31 20:29:41 +0900

    TaskController 클래스 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "Hello, world!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
