8dd27a7ee8e071f764b587976639c176212d5aa8
shineoov <shineoov88@gmail.com>
2022-04-01 00:55:50 +0900

    docs: TaskRepsitory javadoc 수정

--- app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *  {@link Task} 저장소
+ *  해야할 일을 메모리에서 관리합니다.
  */
 @Repository
 public class TaskRepository {
@@ -17,8 +17,8 @@ public class TaskRepository {
     private final Map<Long, Task> tasks = new HashMap<>();
 
     /**
-     * 엔티티를 저장합니다.
-     * @param task 저장할 엔티티
+     * 해야할 일을 저장합니다.
+     * @param task 저장될 할일
      */
     public void save(final Task task) {
         Long taskId = generateId();
@@ -38,15 +38,15 @@ public class TaskRepository {
     }
 
     /**
-     * 모든 엔티티를 리턴합니다.
+     * 해야 할 일 목록을 리턴 합니다.
      */
     public Map<Long, Task> findAll() {
         return tasks;
     }
 
     /**
-     * 주어진 아이디와 일치하는 엔티티를 리턴합니다.
-     * <p>일치하는 엔티티가 없을 경우 {@code null} 을 리턴합니다.</p>
+     * 주어진 아이디와 일치하는 해야 할 일을 리턴합니다.
+     * <p>일치하는 할일이 없을 경우 {@code null} 을 리턴합니다.</p>
      * @param id 아이디
      * @return 일치하는 아이디가 있다면 엔티티, 없다면 {@code null}
      */
@@ -55,8 +55,8 @@ public class TaskRepository {
     }
 
     /**
-     * 저장소에 주어진 아이디와 일치하는 엔티티가 있으면 제거합니다.
-     * <p>주어진 아이디와 일치하는 엔티티가 없다면 아무 일도 일어나지 않습니다.</p>
+     * 주어진 아이디와 일치하는 해야 할 일을 제거합니다.
+     * <p>주어진 아이디와 일치하는 데이터가 없다면 아무 일도 일어나지 않습니다.</p>
      * @param id 아이디
      */
     public void deleteById(final Long id) {
