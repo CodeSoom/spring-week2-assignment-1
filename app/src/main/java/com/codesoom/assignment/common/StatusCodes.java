4a1b4a1462e52020d88569c6eee2234439ca5cbb
5Hanui <gkdud644@naver.com>
2022-04-01 23:36:37 +0900

    404에러 메세지 수정

--- app/src/main/java/com/codesoom/assignment/common/StatusCodes.java
+++ app/src/main/java/com/codesoom/assignment/common/StatusCodes.java
@@ -4,7 +4,7 @@ public enum StatusCodes {
 
     OK(200, ""),
     BAD_REQUEST(400, "잘못된 요청입니다"),
-    NOT_FOUND(404, "요청하신 Task가 존재하지 않습니다");
+    NOT_FOUND(404, "요청한 정보 또는 리소스를 찾을 수 없습니다");
 
     private final int code;
     private final String msg;
