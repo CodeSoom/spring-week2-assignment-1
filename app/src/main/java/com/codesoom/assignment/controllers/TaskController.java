b8db5b8610f63fb86351d4065c99613faa357647
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-03 08:02:51 +0900

    ENH: TaskRepository로 분리

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
+import com.codesoom.assignment.repositories.TaskRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,8 +23,12 @@ import java.util.Optional;
 @RestController
 @RequestMapping("tasks")
 public class TaskController {
-    private List<Task> tasks = new ArrayList<>();
+    private TaskRepository taskRepository;
     private Long newId = 0L;
+
+    TaskController(){
+        this.taskRepository = new TaskRepository();
+    }
 
     private Long generatedId(){
         newId += 1;
@@ -32,21 +37,34 @@ public class TaskController {
 
     @GetMapping()
     public List<Task> getAll() {
-        return tasks;
+        return taskRepository.getTasks();
     }
 
     @PostMapping()
     public ResponseEntity<Task> create(@RequestBody Task task) {
         task.setId(generatedId());
-        tasks.add(task);
+        taskRepository.getTasks().add(task);
         return new ResponseEntity<>(task, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Task> read(@PathVariable("id") Long id) {
-        Optional<Task> task = tasks.stream().filter(t -> t.getId().equals(id))
+        Optional<Task> task = taskRepository.getTasks().stream().filter(t -> t.getId().equals(id))
                 .findFirst();
         if(task.isPresent()){
+            return new ResponseEntity<>(task.get(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private ResponseEntity<Task> getTaskResponseEntity(@RequestBody Task updateTask, @PathVariable("id") Long id) {
+        Optional<Task> task = taskRepository.getTasks().stream().filter(t -> t.getId().equals(id))
+                .findFirst();
+        if(task.isPresent()){
+            if(updateTask != null){
+                task.get().setTitle(updateTask.getTitle());
+            }
             return new ResponseEntity<>(task.get(), HttpStatus.OK);
         }else{
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -55,38 +73,20 @@ public class TaskController {
 
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Task> update(@RequestBody Task updateTask, @PathVariable("id") Long id) {
-        Optional<Task> task = tasks.stream().filter(t -> t.getId().equals(id))
-                .findFirst();
-        if(task.isPresent()){
-            if(updateTask != null){
-                task.get().setTitle(updateTask.getTitle());
-            }
-            return new ResponseEntity<>(task.get(), HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        return getTaskResponseEntity(updateTask, id);
     }
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<Task> partialUpdate(@RequestBody Task updateTask, @PathVariable("id") Long id) {
-        Optional<Task> task = tasks.stream().filter(t -> t.getId().equals(id))
-                .findFirst();
-        if(task.isPresent()){
-            if(updateTask != null){
-                task.get().setTitle(updateTask.getTitle());
-            }
-            return new ResponseEntity<>(task.get(), HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        return getTaskResponseEntity(updateTask, id);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
-        Optional<Task> task = tasks.stream().filter(t -> t.getId().equals(id))
+        Optional<Task> task = taskRepository.getTasks().stream().filter(t -> t.getId().equals(id))
                 .findFirst();
         if(task.isPresent()){
-            tasks.remove(task.get());
+            taskRepository.getTasks().remove(task.get());
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
