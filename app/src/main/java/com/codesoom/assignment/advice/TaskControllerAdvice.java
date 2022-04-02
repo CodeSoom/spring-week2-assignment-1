7e42c50fdab1b149afd56621c047fd7af761a626
shineoov <shineoov88@gmail.com>
2022-04-02 17:35:30 +0900

    refactor: TaskControllerAdvice 속성 변경

--- app/src/main/java/com/codesoom/assignment/advice/TaskControllerAdvice.java
+++ app/src/main/java/com/codesoom/assignment/advice/TaskControllerAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 할 일에 대한 HTTP 요청 중 예외에 대해 응답합니다.
  */
-@RestControllerAdvice(assignableTypes = TaskController.class)
+@RestControllerAdvice(basePackageClasses = TaskController.class)
 public class TaskControllerAdvice {
 
     @ExceptionHandler
