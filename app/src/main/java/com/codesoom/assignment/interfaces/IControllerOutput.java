ffcc2c4a94b991d1747802c7c800da9736d250d6
MJbae <bmj5208@gmail.com>
2022-05-09 21:02:21 +0900

    feat: ControllerOutput 객체에 대한 인터페이스 정의

---
+++ app/src/main/java/com/codesoom/assignment/interfaces/IControllerOutput.java
@@ -0,0 +1,16 @@
+package com.codesoom.assignment.interfaces;
+
+import com.codesoom.assignment.controllers.dtos.*;
+
+public interface IControllerOutput {
+
+    void create(TaskRequestDtoCreating requestDto);
+
+    void update(TaskRequestDtoUpdating requestDto);
+
+    void deleteBy(Long id);
+
+    TaskResponseDto responseDtoSaved();
+
+    TaskResponseDto responseDtoUpdated();
+}
