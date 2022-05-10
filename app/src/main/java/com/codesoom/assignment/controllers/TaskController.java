fe045e131d3b6f954c2bb80a7dd8fb459fe17434
HanJiyoung <jiyounghaan@gmail.com>
2022-05-10 19:42:28 +0900

    TaskController 클래스를 생성하고, Task 목록을 얻어오는 코드를 작성하라

---
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -0,0 +1,20 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private List<Task> tasks = new ArrayList<>();
+
+    @GetMapping
+    public List<Task> getTaskList() {
+        return tasks;
+    }
+}
