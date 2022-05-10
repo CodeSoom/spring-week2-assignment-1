3cf5e6a9e4024958cae32527ee03bedb9f787901
MJbae <bmj5208@gmail.com>
2022-05-10 11:09:52 +0900

    feat: 유지보수성을 높이기 위해 Task 객체 처리에 따라 HTTP Response에 필요한 부분만 TaskResponseDto로 정의

--- app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskResponseDto.java
+++ app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskResponseDto.java
@@ -1,4 +1,16 @@
 package com.codesoom.assignment.controllers.dtos;
 
+import com.codesoom.assignment.Task;
+
 public class TaskResponseDto {
+    private final String title;
+
+    public TaskResponseDto(Task task) {
+        this.title = task.title();
+    }
+
+    public String title() {
+        return title;
+    }
+
 }
