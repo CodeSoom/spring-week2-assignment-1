ae459f05dd21d595be78a982213bea859f473fb7
HanJiyoung <jiyounghaan@gmail.com>
2022-05-10 20:06:59 +0900

    TaskController에 Task를 생성하는 코드를 작성하라
    - Task 생성 시 title이 비어있으면 응답 코드 400을 반환하도록 하였습니다.
    - Task가 올바르게 생성된 후에는 응답 코드 201을 반환하도록 하였습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,10 +1,9 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +11,29 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskController {
     private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
 
     @GetMapping
     public List<Task> getTaskList() {
         return tasks;
+    }
+
+    @PostMapping
+    public Object createTask(@RequestBody Task task, HttpServletResponse response) {
+        if(task.getTitle().isBlank()) {
+            response.setStatus(400);
+            return "";
+        }
+        else {
+            task.setId(generateId());
+            tasks.add(task);
+            response.setStatus(201);
+            return task;
+        }
+    }
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
     }
 }
