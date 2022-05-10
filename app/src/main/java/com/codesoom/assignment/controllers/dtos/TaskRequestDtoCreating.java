b6c594bf311db82aec764e7ce748d6f8c5ebcdf2
MJbae <bmj5208@gmail.com>
2022-05-10 18:37:39 +0900

    feat: TaskRequestDtoCreating 구현

--- app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskRequestDtoCreating.java
+++ app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskRequestDtoCreating.java
@@ -1,4 +1,22 @@
 package com.codesoom.assignment.controllers.dtos;
 
+import com.codesoom.assignment.Task;
+
 public class TaskRequestDtoCreating {
+    private String title;
+
+    TaskRequestDtoCreating() {
+    }
+
+    public TaskRequestDtoCreating(Task task) {
+        this.title = task.title();
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public Task toEntity() {
+        return new Task(title);
+    }
 }
