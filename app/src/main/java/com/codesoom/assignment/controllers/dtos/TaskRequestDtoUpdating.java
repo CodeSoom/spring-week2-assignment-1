7d943e1cd3d6e3fec8d913542af88ca3753038a6
MJbae <bmj5208@gmail.com>
2022-05-10 18:37:52 +0900

    feat: TaskRequestDtoUpdating 구현

--- app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskRequestDtoUpdating.java
+++ app/src/main/java/com/codesoom/assignment/controllers/dtos/TaskRequestDtoUpdating.java
@@ -1,4 +1,21 @@
 package com.codesoom.assignment.controllers.dtos;
 
+import com.codesoom.assignment.Task;
+
 public class TaskRequestDtoUpdating {
+    private final Long id;
+    private final String title;
+
+    public TaskRequestDtoUpdating(Long id, Task task) {
+        this.id = id;
+        this.title = task.title();
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public Task toEntity(){
+        return new Task(title);
+    }
 }
