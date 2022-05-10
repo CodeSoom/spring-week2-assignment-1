2792aeaf0d3d008d03a3c23ae1a0886fc64ec480
MJbae <bmj5208@gmail.com>
2022-05-10 10:57:56 +0900

    feat: DefaultRepository 인터페이스에 따라 TaskRepository 구현

--- app/src/main/java/com/codesoom/assignment/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/TaskRepository.java
@@ -1,4 +1,35 @@
 package com.codesoom.assignment;
 
-public class TaskRepository {
+import com.codesoom.assignment.interfaces.DefaultRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TaskRepository implements DefaultRepository {
+    static private Long sequence = 1L;
+    static private Map<Long, Task> tasks;
+
+    TaskRepository() {
+        tasks = new HashMap<>();
+    }
+
+    @Override
+    public List<Task> tasksAll() {
+        return tasks.values().stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Task taskBy(Long id) {
+        return tasks.values().stream()
+                .filter(task -> Objects.equals(task.id(), id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("taskId(" + id + ")에 해당하는 Task를 Repository에서 찾을 수 없습니다"));
+    }
+
+    @Override
+    public RepositoryOutput output() {
+        return null;
+    }
 }
