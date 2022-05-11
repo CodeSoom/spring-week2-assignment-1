487c7c2b7d2277ab75056694809e360d7cbf0107
10M <endlesslhk@gmail.com>
2022-05-11 15:51:39 +0900

    feat(TaskController): 아이디로 할일 수정 및 삭제 기능 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,11 +7,13 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,13 +24,14 @@ public class TaskController {
     private static int id = 0;
 
     @GetMapping
-    public List<Task> taskList() {
-        return tasks;
+    public ResponseEntity<List<Task>> taskList() {
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Task> task(@PathVariable int id) {
-        Optional<Task> findTask = tasks.stream().filter(x->x.getId()==id).findFirst();
+        Optional<Task> findTask = tasks.stream().filter(x -> x.getId() == id).findFirst();
 
         if (findTask.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -43,6 +46,33 @@ public class TaskController {
         tasks.add(task);
 
         return task;
+    }
+
+    @RequestMapping(path = "/{id}", method = {RequestMethod.PATCH, RequestMethod.PUT})
+    public ResponseEntity<Task> editTask(@PathVariable int id, @RequestBody Task task) {
+        Optional<Task> findTask = tasks.stream().filter(x -> x.getId() == id).findFirst();
+
+        if (findTask.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Task newTask = findTask.get();
+        newTask.setTitle(task.getTitle());
+
+        return new ResponseEntity<>(newTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity deleteTask(@PathVariable int id) {
+        Optional<Task> findTask = tasks.stream().filter(x -> x.getId() == id).findFirst();
+
+        if (findTask.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        tasks.remove(findTask.get()); // get()으로 꼭 가져오기
+
+        return ResponseEntity.noContent().build();
     }
 
     private int getId() {
