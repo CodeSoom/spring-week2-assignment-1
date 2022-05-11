c6623af74a9d9df5b632f146cbc2d67b0b908ae3
10M <endlesslhk@gmail.com>
2022-05-11 15:27:29 +0900

    feat(TaskController): 아이디로 할일 가져오기 함수 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -3,8 +3,12 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.models.Task;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,17 @@ public class TaskController {
     @GetMapping
     public List<Task> taskList() {
         return tasks;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Task> task(@PathVariable int id) {
+        Optional<Task> findTask = tasks.stream().filter(x->x.getId()==id).findFirst();
+
+        if (findTask.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<>(findTask.get(), HttpStatus.OK);
     }
 
     @PostMapping
