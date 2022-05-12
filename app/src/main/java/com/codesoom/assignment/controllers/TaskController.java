61adc3683a5082c3e5c8b63d5270a6f7d5470f0e
seunglo <devseung9056@gmail.com>
2022-05-13 00:13:47 +0900

    TaskController는 TaskService에게 비지니스 로직을 위임하도록 변경해라

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,8 +1,7 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
-import com.codesoom.assignment.repository.TaskRepository;
+import com.codesoom.assignment.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,40 +12,37 @@ import java.util.List;
 @RestController
 public class TaskController {
 
-    private final TaskRepository taskRepository;
+    private final TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
     @GetMapping
     public List<Task> findTasks() {
-        return taskRepository.findAll();
+        return taskService.findTasks();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task);
+        return taskService.createTask(task);
     }
 
     @GetMapping("/{id}")
     public Task findTask(@PathVariable Long id) {
-        return taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        return taskService.findTask(id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id) {
-        taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
-        taskRepository.delete(id);
+        taskService.deleteTask(id);
     }
 
     @RequestMapping(value = "/{id}", method = {RequestMethod.PATCH, RequestMethod.PUT})
-    public Task updateTask(@PathVariable Long id, @RequestBody Task source) {
-        Task target = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
-        target.update(source);
-        return target;
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
     }
 
 }
