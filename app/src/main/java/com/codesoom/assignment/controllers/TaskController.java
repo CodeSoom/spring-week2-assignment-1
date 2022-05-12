0db04980492e3a6f5968143e961d0f0dc79e7d2e
minjoon_lee <minjoon1995@naver.com>
2022-05-12 11:30:41 +0900

    fix : TaskNotFoundException 500 response를 400으로 바꾸고 로깅 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,22 +1,33 @@
 package com.codesoom.assignment.controllers;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codesoom.assignment.dto.TaskDTO;
 import com.codesoom.assignment.dto.TasksDTO;
 import com.codesoom.assignment.dto.TitleDTO;
+import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.service.TaskService;
+
 
 @RestController
 public class TaskController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	private final TaskService taskService;
 
 	@Autowired
@@ -42,5 +53,11 @@ public class TaskController {
 	@DeleteMapping("/tasks/{id}")
 	public ResponseEntity<TasksDTO> deleteTask(@PathVariable long id) {
 		return ResponseEntity.ok().body(taskService.deleteTasks(id));
+	}
+
+	@ExceptionHandler(value = TaskNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void TaskNoFoundException(TaskNotFoundException taskNotFoundException) {
+		logger.info(String.format("[%s] %s", LocalDateTime.now(), taskNotFoundException.getMessage()));
 	}
 }
