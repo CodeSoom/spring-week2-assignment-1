77d654881ef7e3cbc65ade725bb09a664b3f20d8
hyde0401 <hyde0401@nm-patner.com>
2022-05-13 12:37:18 +0900

    style : TODO 작성.(ControllerAdvice)

--- app/src/main/java/com/codesoom/assignment/exception/TaskException.java
+++ app/src/main/java/com/codesoom/assignment/exception/TaskException.java
@@ -5,6 +5,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class TaskException extends ResponseStatusException {
 
+    // TODO Task 객체의 getter/setter 리펙토링 후, ControllerAdvice 적용해볼 것
     public TaskException(HttpStatus status, String reason) {
         super(status, reason);
     }
