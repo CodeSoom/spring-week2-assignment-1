37ba46c412ef6c112fb955027965f5a60f2c9099
hyde0401 <hyde0401@nm-patner.com>
2022-05-13 16:49:27 +0900

    refactor : @ControllerAdvice -> @RestControllerAdvice, ResponseEntity 반환 -> @ResponseStatus + String 반환으로 변경.

--- app/src/main/java/com/codesoom/assignment/controller/TaskExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/controller/TaskExceptionHandler.java
@@ -7,20 +7,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TaskExceptionHandler {
 
     public final String TASK_NOT_FOUND = "할 일을 찾을 수 없습니다. 입력하신 할 일의 id를 확인해주세요.";
     public final String INVALID_TITLE = "할 일의 제목을 확인해주세요";
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity handleNotFoundException() {
-        return new ResponseEntity(TASK_NOT_FOUND, HttpStatus.NOT_FOUND);
+    public String handleNotFoundException() {
+        return TASK_NOT_FOUND;
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(TaskInvalidTitleException.class)
-    public ResponseEntity handleTaskInvalidTitleException() {
-        return new ResponseEntity(INVALID_TITLE, HttpStatus.BAD_REQUEST);
+    public String handleTaskInvalidTitleException() {
+        return INVALID_TITLE;
     }
 }
