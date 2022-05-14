29cc909b1d5e0a0df6b884a17998e93fa4e26e65
HanJiyoung <jiyounghaan@gmail.com>
2022-05-14 21:38:28 +0900

    예외 클래스의 불필요한 주석들을 제거하라
    예외 클래스의 메서드 중 다른 곳에서 호출되지 않아 주석 처리 해놨던 불필요한 메서드들을 삭제했습니다.

--- app/src/main/java/com/codesoom/assignment/exceptions/BadRequestException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/BadRequestException.java
@@ -8,16 +8,4 @@ public class BadRequestException extends RuntimeException {
     public BadRequestException() {
         super();
     }
-
-//    public BadRequestException(String message) {
-//        super(message);
-//    }
-//
-//    public BadRequestException(String message, Throwable cause) {
-//        super(message, cause);
-//    }
-//
-//    public BadRequestException(Throwable cause) {
-//        super(cause);
-//    }
 }
--- app/src/main/java/com/codesoom/assignment/exceptions/NotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/NotFoundException.java
@@ -8,16 +8,4 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException() {
         super();
     }
-
-//    public NotFoundException(String message) {
-//        super(message);
-//    }
-//
-//    public NotFoundException(String message, Throwable cause) {
-//        super(message, cause);
-//    }
-//
-//    public NotFoundException(Throwable cause) {
-//        super(cause);
-//    }
 }
