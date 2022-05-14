3d31ffb116a502e6f9d38378efe2c45740643144
caoyu-dev <choyumin01@gmail.com>
2022-05-14 22:39:39 +0900

    feature(TaskService): service 추가 이전 controller 내용에서 분리함

---
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -0,0 +1,55 @@
+package com.codesoom.assignment.service;
+
+import com.codesoom.assignment.exceptions.TaskNotFoundException;
+import com.codesoom.assignment.models.Task;
+import com.codesoom.assignment.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final static List<Task> tasks = new ArrayList<>();
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task update(Long id, Task newTask) {
+        notFoundException(id);
+        return taskRepository.update(id, newTask);
+    }
+
+    public void delete(Long id) {
+        notFoundException(id);
+        taskRepository.delete(id);
+    }
+
+    private void notFoundException(Long id) {
+        taskRepository.findById(id)
+                .orElseThrow(TaskNotFoundException::new);
+    }
+
+//    private Task findTask(Long id) {
+//        return tasks.stream()
+//                .filter(task -> task.getId() == id)
+//                .findFirst()
+//                .orElseThrow(TaskNotFoundException::new);
+    }
+}
