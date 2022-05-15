019a8dc80856214d6f95da15998ab6159576f9e8
yongjunleeme <chesidyong@gmail.com>
2022-05-15 17:08:57 +0900

    refactor : 인터페이스 주석 추가

--- app/src/main/java/com/codesoom/assignment/services/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/services/TaskService.java
@@ -6,13 +6,39 @@ import java.util.List;
 
 public interface TaskService {
 
+    /**
+     * 모든 할 일을 리턴합니다.
+     */
     List<Task> getTasks();
 
+    /**
+     * 주어진 할 일을 저장한 뒤, 저장된 할 일을 리턴합니다.
+     *
+     * @param task 저장 하고자 하는 할 일
+     * @return 저장 된 할 일
+     */
     Task addTask(Task task);
 
+    /**
+     * 주어진 id에 해당하는 할 일을 찾아 리턴하거나 찾지 못한다면 TaskNotFoundException 예외가 발생합니다.
+     *
+     * @param id 찾고자 하는 할 일의 id
+     * @return 주어진 id에 해당하는 할 일
+     */
     Task getTask(Long id);
 
+    /**
+     * 주어진 id에 해당하는 할 일을 찾아 수정하거나 찾지 못한다면 TaskNotFoundException 예외가 발생합니다.
+     *
+     * @param id 수정하고자 하는 할 일의 id
+     * @return 수정된 할 일
+     */
     Task updateTask(Long id, Task newTask);
 
+    /**
+     * 주어진 id에 해당하는 할 일을 찾아 삭제하거나 찾지 못한다면 TaskNotFoundException 예외가 발생합니다.
+     *
+     * @param id 삭제하고자 하는 할 일의 id
+     */
     void deleteTask(Long id);
 }
