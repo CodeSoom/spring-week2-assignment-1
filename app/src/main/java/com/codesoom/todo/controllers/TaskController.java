55b21b6e9c7f7c53b671657fdd0120e9a1ad9cb9
unajun <bjyoon513@gmail.com>
2022-06-15 00:22:13 +0900

    feat: implements task controllers including 404 exception

--- app/src/main/java/com/codesoom/todo/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/todo/controllers/TaskController.java
@@ -1,6 +1,9 @@
 package com.codesoom.todo.controllers;
 
 import com.codesoom.todo.domain.Task;
+import com.codesoom.todo.services.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -8,41 +11,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/tasks")
 public class TaskController {
+    TaskService taskService = new TaskService();
 
     // TODO: fill skeleton mapper
-    @GetMapping("")
-    public String showTasks() {
-        return "list";
+    @GetMapping("/tasks")
+    public List<Task> showTasks() {
+        return taskService.showTasks();
     }
 
-    @GetMapping("/{id}")
-    public String showTask(@PathVariable("id") int taskId) {
-        return String.valueOf(taskId);
+    @GetMapping("/tasks/{id}")
+    public Task showTask(@PathVariable("id") Long taskId) {
+        return taskService.showTask(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
     }
 
-    @PostMapping("")
-    public Task create (@RequestBody Task task){
-        return task;
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.addTask(task);
     }
 
-    @PutMapping("/{id}")
-    @PatchMapping("/{id}")
-    public Task editTitle (@PathVariable ("id") int taskId){
-        return new Task();
+    @PutMapping("/tasks/{id}")
+    @PatchMapping("/tasks/{id}")
+    public ResponseEntity<Task> editTitle(@PathVariable("id") Long taskId, @RequestBody Task task) {
+        task.setId(taskId);
+        return taskService.editTaskTitle(task)
+                .map(t -> ResponseEntity.status(HttpStatus.CREATED).body(t))
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
     }
 
-    @DeleteMapping("/{id}")
-    public Task deleteTask (@PathVariable("id") int taskId){
-        return new Task();
+    @DeleteMapping("/tasks/{id}")
+    public Task deleteTask(@PathVariable("id") Long taskId) {
+        return taskService.removeTask(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
     }
-
-
 
 
 }
