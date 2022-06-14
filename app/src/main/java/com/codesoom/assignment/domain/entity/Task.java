89acec9997631e275e58a3440425e80fec59ecc7
Jinu Noh <yes.jinu@gmail.com>
2022-06-14 20:39:48 +0900

    feat: make Task class into Entity

--- app/src/main/java/com/codesoom/assignment/domain/entity/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/entity/Task.java
@@ -1,8 +1,18 @@
 package com.codesoom.assignment.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Long id;
 
+    @Column(columnDefinition = "VARCHAR(255)", name="", nullable = false)
     private final String task;
 
     public Task(String task) {
@@ -10,7 +20,12 @@ public class Task {
         this.task = task;
     }
 
+    public Task() {
+        this.id = null;
+        this.task = null;
+    }
+
     public String getTask() {
-        return task;
+        return this.task;
     }
 }
