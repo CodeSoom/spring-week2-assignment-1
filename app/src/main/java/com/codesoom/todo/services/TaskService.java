86317ef9a8937212199234547491828f0decaab7
unajun <bjyoon513@gmail.com>
2022-06-15 00:58:11 +0900

    refactor: run formatter

--- app/src/main/java/com/codesoom/todo/services/TaskService.java
+++ app/src/main/java/com/codesoom/todo/services/TaskService.java
@@ -10,24 +10,27 @@ public class TaskService {
     private final TaskRepository taskRepository = new TaskRepository();
 
 
-    public Task addTask(Task task){
+    public Task addTask(Task task) {
         taskRepository.add(task);
         return task;
     }
 
-    public Optional<Task> showTask(Long id){
+    public Optional<Task> showTask(Long id) {
         return taskRepository.findById(id);
     }
 
-    public List<Task> showTasks(){
+    public List<Task> showTasks() {
         return taskRepository.findAll();
     }
 
-    public Optional<Task> editTaskTitle(Task newTask){
+    // TODO: Javadoc
+    public Optional<Task> editTaskTitle(Task newTask) {
         return taskRepository.edit(newTask);
     }
 
-    public Optional<Task> removeTask(Long id){
-        return taskRepository.findById(id);
+    // Q: delete 의 경우는 키가 없을 수도 있기 리포지토리 단에서 Optional 을 리턴하게 됩니다. 만약 이렇게 하게 된다면 서비스에서 Nested Optional 을 사용하게 되는 문제가 발생하는데 어떻게 해결하는게 좋을까요?
+    // TODO: Javadoc
+    public Optional<Task> removeTask(Long id) {
+        return taskRepository.findById(id).map(t -> {return taskRepository.delete(t););
     }
 }
