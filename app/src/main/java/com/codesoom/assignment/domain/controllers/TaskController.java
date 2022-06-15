05ee41fb28d560a22816d9af32c4eec3ad0da3bc
Jinu Noh <yes.jinu@gmail.com>
2022-06-15 20:02:02 +0900

    fix: fix bug in using annotation parsing path variable

--- app/src/main/java/com/codesoom/assignment/domain/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/domain/controllers/TaskController.java
@@ -6,13 +6,13 @@ import com.codesoom.assignment.domain.service.TaskService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,7 +34,7 @@ public class TaskController {
     }
 
     @GetMapping("{id}")
-    public TaskDTO getTask(@PathParam("id") Long id) {
+    public TaskDTO getTask(@PathVariable Long id) {
         Optional<Task> task = this.taskService.getTask(id);
         if (!task.isPresent()) {
             return null; // 404 response
@@ -53,18 +53,18 @@ public class TaskController {
 
     @PutMapping("{id}")
     @PatchMapping("{id}")
-    public TaskDTO modifyTask(@PathParam("id") Long id, @RequestBody TaskDTO taskDTO) {
         Task modifiedTask = this.taskService.modifyTask(id, taskDTO.getTitle());
+    public TaskDTO modifyTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
 
         return TaskDTO.from(modifiedTask);
     }
 
 
     @DeleteMapping("{id}")
-    public TaskDTO deleteTask(@PathParam("id") Long id) {
         Task deletedTask = this.taskService.deleteTask(id);
         
         return TaskDTO.from(deletedTask);
+    public TaskDTO deleteTask(@PathVariable Long id) {
     }
 
 }
