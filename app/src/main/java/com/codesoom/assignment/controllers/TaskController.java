b78c828700a6608052278b869c135f9a0f403e80
Tyrion <orderhero.tyrionlannister@gmail.com>
2022-06-17 09:29:35 +0900

    Marko 설치 완료 및 본격 수정 준비
    말썽을 부린 마르코를 설치 하고 정상작동 확인 하였음

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,20 +1,35 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
 
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
     @GetMapping
-    public List<Task> getTasks(@RequestParam Long id){
-        return null;
-        //return task;
+    public List<Task> getTasks(){
+        return tasks;
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody Task task){
+
+        task.setId(generateId());
+        tasks.add(task);
+
+        return task;
+    }
+
+    private Long generateId() {
+        return newId++;
     }
 }
