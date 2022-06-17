34fea2ebd887aa2eaf22a70ebc7fa4480194f46f
Tyrion <orderhero.tyrionlannister@gmail.com>
2022-06-17 11:13:10 +0900

    기본 동작이 돌아가게끔 함
    정상적인 입력일 때 정상적으로 출력 한다.
    예외상황에는 대비하지 않음

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -5,7 +5,9 @@ import org.checkerframework.checker.units.qual.A;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,20 +15,46 @@ import java.util.List;
 public class TaskController {
 
     private List<Task> tasks = new ArrayList<>();
+
+    private HashMap<Long, Task> taskHashMap = new HashMap<Long, Task>();
+
     private Long newId = 0L;
 
     @GetMapping
     public List<Task> getTasks(){
-        return tasks;
+
+        List<Task> taskList = new ArrayList<>(taskHashMap.values());
+
+        return taskList;
     }
+
+    @GetMapping("/taskId")
+    public Task getTask(@PathVariable Long taskId){
+
+        return taskHashMap.get(taskId);
+    }
+
 
     @PostMapping
     public Task addTask(@RequestBody Task task){
 
-        task.setId(generateId());
-        tasks.add(task);
+        Long taskId = generateId();
+
+        task.setId(taskId);
+
+        taskHashMap.put(taskId, task);
 
         return task;
+    }
+
+    @PatchMapping("/{taskId}")
+    public void modifyTask(@RequestBody Task task, @PathVariable Long taskId){
+        taskHashMap.get(taskId).setTitle(task.getTitle());
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable Long taskId){
+        taskHashMap.remove(taskId);
     }
 
     private Long generateId() {
