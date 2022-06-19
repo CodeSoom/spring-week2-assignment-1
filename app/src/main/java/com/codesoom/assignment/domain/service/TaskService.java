bd3875dea390ba5f2dbad33ef12db3d37cfc00c2
Noh Jinu <yes.jinu@gmail.com>
2022-06-19 14:07:22 +0900

    docs: add more contexts in error messages

--- app/src/main/java/com/codesoom/assignment/domain/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/domain/service/TaskService.java
@@ -36,7 +36,7 @@ public class TaskService {
     public Task modifyTask(Long id, String newTitle) {
         Optional<Task> task = this.taskRepository.findById(id);
         if (!task.isPresent()) {
-            throw new ResourceNotFoundException("Not found with ID " + id);
+            throw new ResourceNotFoundException("FAILED modifying task, because task with ID " + id + " NOT found");
         }
 
         Task modifiedTask = task.get();
@@ -47,7 +47,7 @@ public class TaskService {
     public void deleteTask(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
         if (!task.isPresent()) {
-            throw new ResourceNotFoundException("Not found with ID " + id);
+            throw new ResourceNotFoundException("FAILED deleting task, because task with ID " + id + " NOT found");
         }
 
         this.taskRepository.delete(task.get());
