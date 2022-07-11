3e45182563e0ad6848eaa676324b4f6df0ac86a7
박범진 <qjawlsqjacks@naver.com>
2022-07-11 21:00:11 +0900

    Task와 관련한 로직을 처리하는 TaskService 생성

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -1,12 +1,13 @@
 package com.codesoom.assignment.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/tasks")
 public class HelloController {
-    @RequestMapping("/")
-    public String sayHello() {
+    @GetMapping("/{taskId}")
+    public String getTask(@PathVariable Long taskId) {
         return "안녕하세요, 리뷰어님. 이번주도 잘 부탁드립니다.";
     }
 }
--- app/src/main/java/com/codesoom/assignment/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -1,2 +1,29 @@
-package com.codesoom.assignment.service;public class TaskService {
+package com.codesoom.assignment.service;
+
+import com.codesoom.assignment.domain.Task;
+import com.codesoom.assignment.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task getTask(Long taskId) {
+        return taskRepository.get(taskId);
+    }
+
+    public Task createTask(String title) {
+        return taskRepository.add(title);
+    }
+
+    public void deleteTask(Long taskId) {
+        taskRepository.remove(taskId);
+    }
+
 }
