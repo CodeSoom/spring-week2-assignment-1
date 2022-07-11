7967516fa8930d4bdc1841706851f18133ac37a9
Ernest Hong <hsh3592@gmail.com>
2022-07-11 21:22:39 +0900

    feat: TaskRepository 사용

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,11 +1,14 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.TaskRepository;
 import com.codesoom.assignment.models.Task;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,24 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    private final List<Task> tasks = new ArrayList<>();
+    private final TaskRepository repository = new TaskRepository();
     private Long newId = 0L;
 
     @GetMapping
     public List<Task> list() {
-        return tasks;
+        return repository.getTasks();
     }
 
     @GetMapping(path="/{taskId}")
     public ResponseEntity<Task> getTask(@PathVariable Long taskId) {
-        return tasks
-                .stream()
-                .filter(task -> task.getId().equals(taskId))
-                .findFirst()
+        return repository.getTaskById(taskId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
@@ -40,9 +41,15 @@ public class TaskController {
     @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task) {
         task.setId(generateId());
-        tasks.add(task);
+        repository.addTask(task);
 
         return task;
+    }
+
+    @PutMapping(path="/{taskId}")
+    @PatchMapping(path="/{taskId}")
+    public void edit(@PathVariable Long taskId, @RequestParam String title) {
+        
     }
 
     private Long generateId() {
