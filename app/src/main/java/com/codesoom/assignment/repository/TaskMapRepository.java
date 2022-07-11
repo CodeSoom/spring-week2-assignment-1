a5efa112b9664386eb25003ad6fe033dafe07311
박범진 <qjawlsqjacks@fasoo.com>
2022-07-12 08:44:42 +0900

    TaskRepository 인터페이스 생성, 기존 TaskRepository 자료구조에 종속된 명칭으로 변경, JavaDoc 존칭 통일

--- app/src/main/java/com/codesoom/assignment/repository/TaskMapRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskMapRepository.java
@@ -1,19 +1,28 @@
 package com.codesoom.assignment.repository;
 
 import com.codesoom.assignment.domain.Task;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 자료구조를 저장하는 방식이 다른 저장소
- * Task를 삽입, 삭제, 조회하는 기능을 가지고 있습니다.
+ * Task를 생성, 삭제, 조회하는 저장소입니다.
  */
-public interface TaskRepository {
+@Repository
+public class TaskMapRepository implements TaskRepository{
+    private Long id = 0L;
+    private final Map<Long, Task> taskMap = new HashMap<>();
+
     /**
      * 요청된 숫자 형식의 taskId와 같은 id를 가진 Task를 찾아 리턴합니다.
      *
      * @param taskId 요청된 숫자 형식의 taskId
      * @return taskId와 같은 id를 가진 Task 리턴
      */
-    Task get(Long taskId);
+    public Task get(Long taskId) {
+        return taskMap.get(taskId);
+    }
 
     /**
      * 입력 받은 title을 가진 Task를 생성해서 리턴합니다.
@@ -21,12 +30,18 @@ public interface TaskRepository {
      * @param title 입력 받은 title
      * @return title을 가진 Task 생성 후 리턴
      */
-    Task add(String title);
+    public Task add(String title) {
+        Task task = new Task(id, title);
+        taskMap.put(id++, task);
+        return task;
+    }
 
     /**
      * 입력 받은 숫자 형식의 taskId와 같은 id를 가진 Task를 찾아 있으면 제거합니다.
      *
      * @param taskId 입력 받은 숫자 형식의 taskId
      */
-    void remove(Long taskId);
+    public void remove(Long taskId) {
+        taskMap.remove(taskId);
+    }
 }
--- app/src/main/java/com/codesoom/assignment/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -1,16 +1,16 @@
 package com.codesoom.assignment.service;
 
 import com.codesoom.assignment.domain.Task;
-import com.codesoom.assignment.repository.TaskRepository;
+import com.codesoom.assignment.repository.TaskMapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
-    private final TaskRepository taskRepository;
+    private final TaskMapRepository taskRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskMapRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
