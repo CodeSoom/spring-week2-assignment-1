94c5929d1239ad937820335a0ea21dab212d3c76
Ernest Hong <hsh3592@gmail.com>
2022-07-11 21:17:06 +0900

    feat: 상세 조회하기할 때 not found 표현하기 위해서 ResponseEntity 사용

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,12 +1,15 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.models.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,15 +27,17 @@ public class TaskController {
     }
 
     @GetMapping(path="/{taskId}")
-    public Task getTask(@PathVariable Long taskId) {
+    public ResponseEntity<Task> getTask(@PathVariable Long taskId) {
         return tasks
                 .stream()
                 .filter(task -> task.getId().equals(taskId))
                 .findFirst()
-                .orElse(null);
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task) {
         task.setId(generateId());
         tasks.add(task);
