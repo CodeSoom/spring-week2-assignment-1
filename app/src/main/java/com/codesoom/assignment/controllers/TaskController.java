2012e88e9df0dc4ed029ec9bc235272d324a2209
eunice0035 <eunice7357@gmail.com>
2022-07-11 20:12:31 +0900

    REST API: ToDo  목록얻기 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,2 +1,18 @@
-package com.codesoom.assignment.controllers;public class TaskController {
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.models.Task;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private HashMap<Long, String> tasks = new HashMap<>();
+    
+    @GetMapping
+    public HashMap<Long, String> list(){
+        return tasks;
+    }
+
 }
+
