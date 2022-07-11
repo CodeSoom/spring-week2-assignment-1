ef643b1577f3ea24dc523be167fb97afa1e2acba
Ernest Hong <hsh3592@gmail.com>
2022-07-11 21:45:30 +0900

    refactor: 사용하지 않는 import 제거

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
