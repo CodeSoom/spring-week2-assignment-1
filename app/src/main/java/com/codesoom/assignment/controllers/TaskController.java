939759cc22e71025f47555d1ebfdb0a83d10ca1f
eunice0035 <eunice7357@gmail.com>
2022-07-11 20:13:48 +0900

    REST API: ToDo 생성하기 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -8,11 +8,23 @@ import java.util.HashMap;
 @RequestMapping("/tasks")
 public class TaskController {
     private HashMap<Long, String> tasks = new HashMap<>();
-    
+    private Long curTaskID = 0L;
+
     @GetMapping
     public HashMap<Long, String> list(){
         return tasks;
     }
 
+    @PostMapping
+    public Task create(@RequestBody Task task){
+        task.setId(generateID());
+        tasks.put(task.getId(), task.getTitle());
+        return task;
+    }
+
+    private Long generateID(){
+        curTaskID += 1;
+        return curTaskID;
+    }
 }
 
