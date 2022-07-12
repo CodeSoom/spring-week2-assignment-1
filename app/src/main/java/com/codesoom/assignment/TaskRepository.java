77f095ef2c6a28cedd75c4b887e4b8226449ebe9
Ernest Hong <hsh3592@gmail.com>
2022-07-12 19:42:59 +0900

    docs: TaskRepository 주석 수정

--- app/src/main/java/com/codesoom/assignment/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/TaskRepository.java
@@ -8,7 +8,9 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Task를 저장하는 객체 입니다
+ * Task를 추가, 삭제, 수정, 조회 합니다
+ * 도메인 Layer와 데이터 접근 Layer 사이의 Mediator 역할을 합니다
+ * 현재는 in-memory 방식으로 데이터를 관리합니다
  */
 public class TaskRepository {
     private final List<Task> tasks = Collections.synchronizedList(new ArrayList<>());
