0241fa18d1d0bc31d3e98ea31b126c5ace52c410
박범진 <qjawlsqjacks@fasoo.com>
2022-07-12 16:50:36 +0900

    Task를 수정하는 메서드 생성
    입력 받은 id와 같은 Task가 있을때, title을 변경시켜 리턴하는
    메서드입니다.

--- app/src/main/java/com/codesoom/assignment/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -7,7 +7,7 @@ import java.util.Objects;
  */
 public class Task {
     private final Long id;
-    private final String title;
+    private String title;
 
     public Task(Long id, String title) {
         this.id = id;
@@ -16,6 +16,17 @@ public class Task {
 
     public boolean isSame(Long id) {
         return this.id.equals(id);
+    }
+
+    /**
+     * 입력 받은 title로 현재 title을 바꾸고 바뀐 Task를 리턴한다.
+     *
+     * @param title 입력 받은 문자열 title
+     * @return 바뀐 Task 리턴
+     */
+    public Task changeTitle(String title) {
+        this.title = title;
+        return this;
     }
 
     @Override
--- app/src/main/java/com/codesoom/assignment/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -5,6 +5,11 @@ import com.codesoom.assignment.repository.TaskMapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+/**
+ * Task와 관련된 로직을 수행하는 서비스입니다.
+ */
 @Service
 public class TaskService {
     private final TaskMapRepository taskRepository;
@@ -18,12 +23,26 @@ public class TaskService {
         return taskRepository.get(taskId);
     }
 
+    public List<Task> getAll() {
+        return taskRepository.getAll();
+    }
+
     public Task createTask(String title) {
         return taskRepository.add(title);
+    }
+
+    /**
+     * 입력 받은 숫자 형식의 taskId와 같은 id를 가진 Task가 있으면 입력 받은 문자열 title로 바꾸고 바뀐 Task를 리턴한다.
+     *
+     * @param taskId 입력 받은 숫자 타입의 taskId
+     * @param title 입력 받은 문자열 타입의 title
+     * @return 바뀐 Task 리턴
+     */
+    public Task modifyTask(Long taskId, String title) {
+        return getTask(taskId).changeTitle(title);
     }
 
     public void deleteTask(Long taskId) {
         taskRepository.remove(taskId);
     }
-
 }
