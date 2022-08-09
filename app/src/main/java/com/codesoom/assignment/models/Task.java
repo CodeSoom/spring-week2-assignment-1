da133687110cbc37204226821e30ddca6798e2b1
정현준 <jeongdalma@gmail.com>
2022-08-09 11:13:44 +0900

    refactor: Setter제거 , 필드 private로 변경

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,8 +1,8 @@
 package com.codesoom.assignment.models;
 
 public class Task {
-    public Long id;
-    public String title;
+    private final Long id;
+    private final String title;
 
     public Task(Long id, String title) {
         this.id = id;
@@ -13,16 +13,8 @@ public class Task {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     @Override
