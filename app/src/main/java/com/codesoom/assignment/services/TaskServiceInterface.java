49eb07b3ed868ab5b30811eb9dc108bd9199f528
정현준 <jeongdalma@gmail.com>
2022-08-10 12:36:34 +0900

    TaskServiceInterface void메서드 주석 추가

--- app/src/main/java/com/codesoom/assignment/services/TaskServiceInterface.java
+++ app/src/main/java/com/codesoom/assignment/services/TaskServiceInterface.java
@@ -10,7 +10,14 @@ public interface TaskServiceInterface {
 
     List<Task> getAllTask();
     Task getTaskById(Long id);
+
+    /**
+     * 과제를 새로 추가합니다
+     * @param taskDTO
+     * @return null
+     */
     void createTask(TaskDTO taskDTO);
+
     Task updateTask(Long id , TaskDTO taskDTO);
     Task deleteTask(Long id);
 }
