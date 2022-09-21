a45ed80baade33722b8a776ca2535c0707edce51
Eunha Jeong <eunhaay@gmail.com>
2022-09-21 12:51:27 +0900

    Docs: TaskRepository 주석 추가 및 수정
    - 클래스 상단에 Java doc 추가
    - 주석에 불변성을 갖는 도메인이 드러나도록 수정

--- app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -9,6 +9,9 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 할 일의 저장 및 관리를 담당합니다.
+ */
 @Repository
 public class TaskRepository {
 
@@ -16,8 +19,8 @@ public class TaskRepository {
     private final Map<Long, Task> taskStorage = new ConcurrentHashMap<>();
 
     /**
-     * 새로운 Task 아이디를 생성해 반환합니다.
-     * @return Task 아이디
+     * 새로운 할 일 아이디를 생성해 반환합니다.
+     * @return 할 일 아이디
      */
     private synchronized Long generateId() {
         sequence += 1;
@@ -25,26 +28,26 @@ public class TaskRepository {
     }
 
     /**
-     * 저장된 모든 Task를 반환합니다.
-     * @return Task 목록
+     * 저장된 모든 할 일을 반환합니다.
+     * @return 할 일 전체 목록
      */
     public List<Task> findAll() {
         return new ArrayList<>(taskStorage.values());
     }
 
     /**
-     * 아이디에 해당하는 Task를 찾아 반환합니다.
-     * @param id 조회하려는 Task 아이디
-     * @return 조회된 Task
+     * 아이디에 해당하는 할 일을 찾아 반환합니다.
+     * @param id 조회하려는 할 일 아이디
+     * @return 조회된 할 일
      */
     public Optional<Task> findById(Long id) {
         return Optional.ofNullable(taskStorage.get(id));
     }
 
     /**
-     * 새로운 Task를 생성하고 반환합니다.
-     * @param task 생성할 Task
-     * @return 새로 생성된 Task
+     * 새로운 할 일을 생성하고 반환합니다.
+     * @param task 생성할 할 일 데이터
+     * @return 새로 생성된 할 일
      */
     public Task create(Task task) {
         final Long id = generateId();
@@ -59,10 +62,10 @@ public class TaskRepository {
     }
 
     /**
-     * 기존의 Task를 아이디로 찾아 수정합니다.
-     * @param id 수정할 Task 아이디
-     * @param task 수정할 Task
-     * @return 수정 완료된 Task
+     * 기존의 할 일을 아이디로 찾아 수정합니다.
+     * @param id 수정할 할 일 아이디
+     * @param task 수정할 할 일 데이터
+     * @return 수정 완료된 할 일
      */
     public Task update(Long id, Task task) {
         Task foundTask = findById(id)
@@ -73,9 +76,9 @@ public class TaskRepository {
     }
 
     /**
-     * 아이디에 해당하는 Task를 찾아 삭제합니다.
-     * @param id 삭제할 Task 아이디
-     * @return 삭제한 Task
+     * 아이디에 해당하는 할 일을 찾아 삭제합니다.
+     * @param id 삭제할 할 일 아이디
+     * @return 삭제된 할 일
      */
     public Optional<Task> deleteById(Long id) {
         return Optional.ofNullable(taskStorage.remove(id));
