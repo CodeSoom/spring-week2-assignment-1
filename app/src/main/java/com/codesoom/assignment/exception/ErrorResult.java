4ced9bc4a2b022701f33b638ad92d02c39f48f42
parkcw <luckycw@parkcw.com>
2022-09-21 16:50:43 +0900

    Fix: Delete unnecessary setter method in ErrorResult

--- app/src/main/java/com/codesoom/assignment/exception/ErrorResult.java
+++ app/src/main/java/com/codesoom/assignment/exception/ErrorResult.java
@@ -13,16 +13,8 @@ public class ErrorResult {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     @Override
