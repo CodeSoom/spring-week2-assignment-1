8aaf9890987da5a9fa46383929f42bef0fbeb571
parkcw <luckycw@parkcw.com>
2022-09-23 18:11:09 +0900

    Fix: Add comment in TaskService

--- app/src/main/java/com/codesoom/assignment/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -18,27 +18,57 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    /**
+     * 등록된 모든 할 일들을 리턴한다.
+     * @return 등록된 모든 할 일
+     */
     public List<Task> findAll() {
         return taskRepository.findAll();
     }
 
+    /**
+     * 요청받은 할 일 ID로 해당 할 일을 조회해서 리턴한다.
+     * @param id 요청 할 일 ID
+     * @exception TaskIdNotFoundException 할 일을 못 찾을 경우 발생하는 예외
+     * @return 검색된 할 일
+     */
     public Task findById(Long id) {
         return taskRepository.findById(id).orElseThrow(throwTaskException());
     }
 
+    /**
+     * 클라이언트가 입력한 할 일을 저장하고 저장한 할 일을 리턴한다.
+     * @param task 입력한 할 일
+     * @return 저장된 할 일
+     */
     public Task insertTask(Task task) {
         return taskRepository.save(task);
     }
 
+    /**
+     * 수정할 할 일 ID와 수정내용을 받아서 해당 ID의 할 일을 수정내용으로 수정 후
+     * 수정된 할 일을 리턴한다.
+     * @param id 수정대상 할 일 ID
+     * @param newTask 수정내용
+     * @exception TaskIdNotFoundException 할 일을 못 찾을 경우 발생하는 예외
+     * @return 수정된 할 일
+     */
     public Task updateTask(Long id, UpdateTask newTask) {
         return taskRepository.update(id, newTask).orElseThrow(throwTaskException());
 
     }
 
+    /**
+     * 삭제할 할 일 ID를 받아서 해당 할 일을 삭제한다.
+     * @param id 삭제할 할 일 ID
+     */
     public void deleteTask(Long id) {
         taskRepository.delete(id).orElseThrow(throwTaskException());
     }
 
+    /**
+     * TaskNotFoundException을 발생시킨다.
+     */
     private Supplier<TaskIdNotFoundException> throwTaskException() {
         return () -> new TaskIdNotFoundException("존재하지않는 아이디입니다.");
     }
