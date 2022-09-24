9c478eaad3e923abc74a9ffb33b6ee87679ec534
yewon9609 <dodnjs1241@gmail.com>
2022-09-25 02:35:08 +0900

    refactoring:메소드 리턴타입 변경및 추가

--- app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -8,11 +8,13 @@ import java.util.List;
 public interface TaskRepository {
 
     public List<Task> findAll();
+
     public ResponseEntity<Task> readTask(Long id);
+
     public Task createTask(Task task);
+
     public ResponseEntity<Task> updateTask(Long id, Task task);
+
     public ResponseEntity<Task> deleteTask(Long id);
-
-
-
 }
+
