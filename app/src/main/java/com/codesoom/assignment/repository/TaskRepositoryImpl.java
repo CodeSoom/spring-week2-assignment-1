88d7614d1ccaf88783193bafd97a4565e7213dc2
yewon9609 <dodnjs1241@gmail.com>
2022-09-25 02:32:16 +0900

    refactoring:각 메소드 리턴값 변경 및 tasks 타입 ConcurrentHashMap으로 변경

--- app/src/main/java/com/codesoom/assignment/repository/TaskRepositoryImpl.java
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepositoryImpl.java
@@ -10,15 +10,15 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
-public class TaskRepositoryImpl implements TaskRepository{
+public class TaskRepositoryImpl implements TaskRepository {
     private final ConcurrentHashMap<Long, Task> tasks = new ConcurrentHashMap<>();
 
-    public List<Task> findAll(){
+    public List<Task> findAll() {
         return new ArrayList<>(tasks.values());
     }
 
     public ResponseEntity<Task> readTask(Long id) {
-        return tasks.containsKey(id)?
+        return tasks.containsKey(id) ?
                 new ResponseEntity<>(tasks.get(id), HttpStatus.OK) : new ResponseEntity<>(new Task(), HttpStatus.NOT_FOUND);
     }
 
@@ -28,22 +28,20 @@ public class TaskRepositoryImpl implements TaskRepository{
         return newTask;
     }
 
-    public ResponseEntity<Task> updateTask(Long id, Task task){
-       if(tasks.containsKey(id)){
-           Task updateTask = tasks.get(id).newTitle(task.getTitle());
-           tasks.put(id,updateTask);
-           return new ResponseEntity<>(updateTask, HttpStatus.OK);
-       }
+    public ResponseEntity<Task> updateTask(Long id, Task task) {
+        if (tasks.containsKey(id)) {
+            Task updateTask = tasks.get(id).newTitle(task.getTitle());
+            tasks.put(id, updateTask);
+            return new ResponseEntity<>(updateTask, HttpStatus.OK);
+        }
         return new ResponseEntity<>(new Task(), HttpStatus.NOT_FOUND);
     }
 
-    public ResponseEntity<Task> deleteTask(Long id){
-        if(tasks.containsKey(id)){
+    public ResponseEntity<Task> deleteTask(Long id) {
+        if (tasks.containsKey(id)) {
             tasks.remove(id);
             return new ResponseEntity<>(tasks.get(id), HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(new Task(), HttpStatus.NOT_FOUND);
     }
-
-
 }
