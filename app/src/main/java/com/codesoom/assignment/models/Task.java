c327ea829983da6128a44bad22b0571007177df5
yewon9609 <dodnjs1241@gmail.com>
2022-09-25 02:18:33 +0900

    refectoring:setTitle()->newTitle()로 변경

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -8,16 +8,20 @@ public class Task {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public Task newTitle(String title) {
+      return new Task(this.getId(), title);
+    }
+
+    public Task(Long id, String title) {
+        this.id = id;
         this.title = title;
+    }
+
+    public Task() {
     }
 
     public String toString() {
