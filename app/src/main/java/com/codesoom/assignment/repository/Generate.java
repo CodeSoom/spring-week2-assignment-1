c964ed10844f4c48843437a946babca03e3c471d
yewon9609 <dodnjs1241@gmail.com>
2022-09-25 02:22:08 +0900

    refectoring:id생성 로직 분리

--- app/src/main/java/com/codesoom/assignment/repository/Generate.java
+++ app/src/main/java/com/codesoom/assignment/repository/Generate.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.repository;
 
 public class Generate {
-    private static Long newId;
+    private static Long newId = 0L;
 
     public static synchronized Long id() {
         newId += 1;
