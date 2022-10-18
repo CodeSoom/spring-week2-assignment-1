f1fd4c441d25eae16ced59b87a19928d09089db5
steve7867 <parkjunsu3@gmail.com>
2022-10-19 02:35:39 +0900

    Task 클래스 regDate, modDate 필드 추가 및 TaskDto setter, getter 수정

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,13 +1,26 @@
 package com.codesoom.assignment.models;
 
+import java.time.LocalDateTime;
+
 public class Task {
 
     private final Long id;
     private final String title;
+    private final LocalDateTime regDate;
+    private final LocalDateTime modDate;
 
-    public Task(Long id, String title) {
+    public Task(Long id, String title, LocalDateTime regDate) {
         this.id = id;
         this.title = title;
+        this.regDate = regDate;
+        this.modDate = regDate;
+    }
+
+    public Task(Long id, String title, LocalDateTime regDate, LocalDateTime modDate) {
+        this.id = id;
+        this.title = title;
+        this.regDate = regDate;
+        this.modDate = modDate;
     }
 
     public Long getId() {
@@ -16,5 +29,13 @@ public class Task {
 
     public String getTitle() {
         return title;
+    }
+
+    public LocalDateTime getRegDate() {
+        return regDate;
+    }
+
+    public LocalDateTime getModDate() {
+        return modDate;
     }
 }
--- app/src/main/java/com/codesoom/assignment/models/TaskDto.java
+++ app/src/main/java/com/codesoom/assignment/models/TaskDto.java
@@ -1,12 +1,13 @@
 package com.codesoom.assignment.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TaskDto {
 
     @JsonProperty private Long id;
     @JsonProperty private String title;
-    private Task task;
+    @JsonIgnore private Task task;
 
     public TaskDto() {
     }
@@ -20,21 +21,20 @@ public class TaskDto {
         setTitle(task.getTitle());
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public void setId(Long id) {
         this.id = id;
-        if (title != null) {
-            task = new Task(id, title);
-        }
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
-        if (id != null) {
-            task = new Task(id, title);
-        }
     }
 
-    public Task getTask() {
-        return task;
-    }
 }
