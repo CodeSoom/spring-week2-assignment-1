06d334527c6462c30fc15dbb7798ec33eb1fcd43
steve7867 <parkjunsu3@gmail.com>
2022-10-19 15:11:15 +0900

    deleteTasks() 추가

--- app/src/main/java/com/codesoom/assignment/repositories/TaskRepositoryImpl.java
+++ app/src/main/java/com/codesoom/assignment/repositories/TaskRepositoryImpl.java
@@ -3,8 +3,10 @@ package com.codesoom.assignment.repositories;
 import com.codesoom.assignment.models.Task;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,5 +33,16 @@ public class TaskRepositoryImpl implements TaskRepository {
     public Optional<Task> deleteById(Long id) {
         final Task removedTask = taskMap.remove(id);
         return Optional.ofNullable(removedTask);
+    }
+
+    public Map<Long, Task> deleteTasks(List<Long> idList) {
+        final Map<Long, Task> removedTasks = new ConcurrentHashMap<>();
+
+        for (Long id : idList) {
+            Task removedTask = taskMap.remove(id);
+            removedTasks.put(id, removedTask);
+        }
+
+        return removedTasks;
     }
 }
