b6513b3af7ff70f621a3c4a8db6c3ec9b8d97d62
giibeom <kpmyung@gmail.com>
2022-10-20 20:50:54 +0900

    feat: CORS 설정 추가

--- app/src/main/java/com/codesoom/assignment/task/controller/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/task/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +23,9 @@ import javax.validation.constraints.Min;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 @Validated
 public class TaskController {
     private final TaskService taskService;
