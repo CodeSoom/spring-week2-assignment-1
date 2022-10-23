b3d1f72d15feb84110c69ebfdd0014593e81252e
steve7867 <parkjunsu3@gmail.com>
2022-10-23 19:46:52 +0900

    NullTaskStoredException 제거

--- app/src/main/java/com/codesoom/assignment/exceptions/NullTaskStoredException.java
+++
@@ -1,7 +0,0 @@
-package com.codesoom.assignment.exceptions;
-
-public class NullTaskStoredException extends RuntimeException {
-    public NullTaskStoredException(String message) {
-        super(message);
-    }
-}
