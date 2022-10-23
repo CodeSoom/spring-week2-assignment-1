4831b8092d6bd68bc2975514490c8d42892974f7
giibeom <kpmyung@gmail.com>
2022-10-23 20:54:24 +0900

    refactor: 실제 예외 생성 객체에서 발생 시간 생성 하도록 변경(CommonException)

--- app/src/main/java/com/codesoom/assignment/exception/CommonException.java
+++ app/src/main/java/com/codesoom/assignment/exception/CommonException.java
@@ -3,6 +3,8 @@ package com.codesoom.assignment.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 /**
  * 직접 생성한 Exception 객체들의 공통 부모입니다.
  * 자식 Exception 으로부터 넘겨받은 status와 message를 RuntimeException으로 전달합니다.
@@ -12,6 +14,7 @@ public class CommonException extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "일시적으로 접속이 원활하지 않습니다. 잠시 후 다시 요청해주시길 바랍니다.";
 
     private final HttpStatus status;
+    private final LocalDateTime occuredTime = LocalDateTime.now();
 
     public CommonException() {
         super(DEFAULT_MESSAGE);
