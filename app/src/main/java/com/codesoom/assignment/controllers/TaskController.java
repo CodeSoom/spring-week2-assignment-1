9dba7a469922deddf7237867c56ffcd202f2dc30
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-14 21:54:34 +0900

    스프링 Annotation을 활용하여 GET/POST/PUT/PATCH/DELETE를 구현해라.
    에러 없는 파라미터가 날라온다는 가정 하에 GET/POST/PUT/PATCH/DELETE 초기 구현 완료함

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,8 +2,11 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.model.Task;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,24 @@ public class TaskController {
         return tasks;
     }
 
+    /**
+     * GET /tasks/{id}
+     * @param id
+     * @return task
+     */
+    @GetMapping("/{id}")
+    public Task getTask(@PathVariable(name = "id") Long id) {
+        return tasks.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * POST /tasks
+     * @param task
+     * @return task
+     */
     @PostMapping
     public Task createList(@RequestBody Task task) {
         task.setTitle(task.getTitle());
@@ -35,6 +56,39 @@ public class TaskController {
         tasks.add(task);
 
         return task;
+    }
+
+    /**
+     * PUT/PATCH /tasks/{id}
+     * @param id
+     * @param task
+     * @return task
+     */
+    @PutMapping("/{id}")
+    @PostMapping("/{id}")
+    public Task updateTask(@PathVariable(name = "id") Long id, @RequestBody Task task) {
+
+        Task filteredTask = tasks.stream()
+                            .filter(t -> t.getId().equals(id))
+                            .findFirst()
+                            .orElse(null);
+        filteredTask.setTitle(task.getTitle());
+
+        return filteredTask;
+    }
+
+    /**
+     * DELETE /tasks/{id}
+     * @param id
+     */
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable(name = "id") Long id) {
+        Task filteredTask = tasks.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        tasks.remove(filteredTask);
     }
 
     /**
