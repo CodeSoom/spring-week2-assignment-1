3042581af6785a7a039fd39ba645ca527a834923
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-14 21:59:12 +0900

    @PutMapping과 @PatchMapping을 분리해라.
    같이 두어도 컴파일 에러가 안나길래 되는 줄 알았으나, 런타임 에러가 발생하여 메서드 따로 분리함.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,15 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.model.Task;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,13 +57,24 @@ public class TaskController {
      * @return task
      */
     @PutMapping("/{id}")
-    @PostMapping("/{id}")
-    public Task updateTask(@PathVariable(name = "id") Long id, @RequestBody Task task) {
+    public Task putTask(@PathVariable(name = "id") Long id, @RequestBody Task task) {
 
         Task filteredTask = tasks.stream()
                             .filter(t -> t.getId().equals(id))
                             .findFirst()
                             .orElse(null);
+        filteredTask.setTitle(task.getTitle());
+
+        return filteredTask;
+    }
+
+    @PatchMapping("/{id}")
+    public Task patchTask(@PathVariable(name = "id") Long id, @RequestBody Task task) {
+
+        Task filteredTask = tasks.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElse(null);
         filteredTask.setTitle(task.getTitle());
 
         return filteredTask;
