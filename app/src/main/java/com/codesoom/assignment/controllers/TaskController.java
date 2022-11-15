768bc72f76e69caac1b1ec75deffc299e8cdcb9e
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-16 01:06:16 +0900

    @ExceptionHandler를 사용하여 예외를 처리하고, ErrorHandler 클래스를 만들어라.
    @ExceptionHandler를 사용하여 각 에러에 해당하는 예외처리함.
    ErrorHandler를 통해 Error code와 message만 사용자에게 보이도록 함.

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -1,7 +1,22 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.handler.ErrorHandler;
 import com.codesoom.assignment.model.Task;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +29,24 @@ public class TaskController {
     private List<Task> tasks = new ArrayList<>();
     private Long id = 0L;
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NullPointerException.class)
+    public ErrorHandler NPEHandler(NullPointerException ne) {
+        return new ErrorHandler("Not Found", ne.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequest.class)
+    public ErrorHandler BadRequestHandler(BadRequest badRequest) {
+        return new ErrorHandler("Bad Request", "다시 한 번 확인해주세요.");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorHandler notReadableHandler(HttpMessageNotReadableException he) {
+        return new ErrorHandler("Not Readable", "title이 존재하지 않습니다. 다시 한 번 확인해주세요.");
+    }
+
     /**
      * GET /tasks
      * @return tasks
@@ -25,15 +58,17 @@ public class TaskController {
 
     /**
      * GET /tasks/{id}
+     * TODO 찾고자 하는 task가 없을 경우 404에러
      * @param id
      * @return task
      */
     @GetMapping("/{id}")
     public Task getTask(@PathVariable(name = "id") Long id) {
+
         return tasks.stream()
                 .filter(task -> task.getId().equals(id))
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new NullPointerException("Task가 존재하지 않습니다. ID를 다시 한 번 확인해주세요. id="+id));
     }
 
     /**
@@ -42,6 +77,7 @@ public class TaskController {
      * @return task
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Task createList(@RequestBody Task task) {
         task.setTitle(task.getTitle());
         task.setId(generateId());
@@ -50,8 +86,9 @@ public class TaskController {
         return task;
     }
 
+    //TODO PUT, PATCH 코드 동일함. 어떻게 처리?
     /**
-     * PUT/PATCH /tasks/{id}
+     * PUT /tasks/{id}
      * @param id
      * @param task
      * @return task
@@ -62,12 +99,21 @@ public class TaskController {
         Task filteredTask = tasks.stream()
                             .filter(t -> t.getId().equals(id))
                             .findFirst()
-                            .orElse(null);
+                            .orElseThrow(() -> new NullPointerException("ID를 다시 한 번 확인해주세요. ID="+id));
+        //NPE 발생 가능
+        if (task.getTitle().isEmpty()) throw new NullPointerException("Title을 입력해주세요.");
+
         filteredTask.setTitle(task.getTitle());
 
         return filteredTask;
     }
 
+    /**
+     * PATCH /tasks/{id}
+     * @param id
+     * @param task
+     * @return task
+     */
     @PatchMapping("/{id}")
     public Task patchTask(@PathVariable(name = "id") Long id, @RequestBody Task task) {
 
@@ -81,15 +127,17 @@ public class TaskController {
     }
 
     /**
+     * TODO HttpStatus NO_CONTENT(204)에 대해 알아보기
      * DELETE /tasks/{id}
      * @param id
      */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable(name = "id") Long id) {
         Task filteredTask = tasks.stream()
                 .filter(t -> t.getId().equals(id))
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new NullPointerException("Task가 존재하지 않습니다. ID를 다시 한 번 확인해주세요. id="+id));
 
         tasks.remove(filteredTask);
     }
