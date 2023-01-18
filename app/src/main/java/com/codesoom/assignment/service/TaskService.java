aba6569ce46589526dfecb5b977deb8078a72df3
GyeongHun <>
2023-01-18 23:32:20 +0900

    Base MVC Pattern
    Rest Api 기본 mvc pattern 구조 작성.

---
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -0,0 +1,49 @@
+package com.codesoom.assignment.service;
+
+import com.codesoom.assignment.model.Task;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskService {
+    private List<Task> tasks = new ArrayList<>();
+    private Long id = 0L;
+
+    public List<Task> getTasks(){
+        return tasks;
+    }
+
+    public Task getTask(Long id){
+        Task task = findTask(id);
+        return task;
+    }
+
+    public Task createTask(Task task){
+        task.setId(id++);
+        tasks.add(task);
+
+        return task;
+    }
+
+    public Task updateTask(Task task){
+        Task updateTask = findTask(task.getId());
+        updateTask.setTitle(task.getTitle());
+
+        return task;
+    }
+
+    public Task deleteTask(Long id){
+        Task deleteTask = findTask(id);
+        tasks.remove(deleteTask);
+
+        return deleteTask;
+    }
+
+    public Task findTask(Long id){
+        return tasks.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);;
+    }
+}
