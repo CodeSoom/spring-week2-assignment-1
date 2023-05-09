b753c83c5ae3dd5f71617f94ec7e9ca61d2ab917
EunjinJwa <ejjwa0563@gmail.com>
2023-05-10 01:14:08 +0900

    Controller Cors 적용 및 marko js view 적용

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -4,18 +4,12 @@ import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.model.Task;
 import com.codesoom.assignment.task.TaskService;
 import java.util.List;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
 
 	private TaskService taskService;
